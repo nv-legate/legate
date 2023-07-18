92319e60cb42895a35695ba4d15ce1ca67810890
Wonchan Lee <wonchanl@nvidia.com>
2023-07-18 16:24:51 -0700

    Update to pass pre-commit hooks

--- tests/cpp/unit/span.cc
+++ tests/cpp/unit/span.cc
@@ -19,7 +19,6 @@
 
 namespace span_test {
 
-
 constexpr bool BOOL_VALUE       = true;
 constexpr int8_t INT8_VALUE     = 10;
 constexpr int16_t INT16_VALUE   = -1000;
@@ -41,7 +40,6 @@ const complex<float> COMPLEX_FLOAT_VALUE3{4, 5};
 const complex<double> COMPLEX_DOUBLE_VALUE1{6, 7};
 const complex<double> COMPLEX_DOUBLE_VALUE2{8, 9};
 const complex<double> COMPLEX_DOUBLE_VALUE3{10, 11};
-
 
 constexpr uint32_t DATA_SIZE = 3;
 
@@ -69,9 +67,12 @@ TEST(SpanUnit, Create)
   create(INT32_VALUE, static_cast<int32_t>(INT32_VALUE + 1), static_cast<int32_t>(INT32_VALUE + 2));
   create(INT64_VALUE, static_cast<int64_t>(INT64_VALUE + 1), static_cast<int64_t>(INT64_VALUE + 2));
   create(UINT8_VALUE, static_cast<uint8_t>(UINT8_VALUE + 1), static_cast<uint8_t>(UINT8_VALUE + 2));
-  create(UINT16_VALUE, static_cast<uint16_t>(UINT16_VALUE + 1), static_cast<uint16_t>(UINT16_VALUE + 2));
-  create(UINT32_VALUE, static_cast<uint32_t>(UINT32_VALUE + 1), static_cast<uint32_t>(UINT32_VALUE + 2));
-  create(UINT64_VALUE, static_cast<uint64_t>(UINT64_VALUE + 1), static_cast<uint64_t>(UINT64_VALUE + 2));
+  create(
+    UINT16_VALUE, static_cast<uint16_t>(UINT16_VALUE + 1), static_cast<uint16_t>(UINT16_VALUE + 2));
+  create(
+    UINT32_VALUE, static_cast<uint32_t>(UINT32_VALUE + 1), static_cast<uint32_t>(UINT32_VALUE + 2));
+  create(
+    UINT64_VALUE, static_cast<uint64_t>(UINT64_VALUE + 1), static_cast<uint64_t>(UINT64_VALUE + 2));
   create(FLOAT_VALUE, FLOAT_VALUE + 1.0f, FLOAT_VALUE + 2.0f);
   create(DOUBLE_VALUE, DOUBLE_VALUE + 1.0d, DOUBLE_VALUE + 2.0d);
   create(STRING_VALUE, STRING_VALUE + "1", STRING_VALUE + "2");
