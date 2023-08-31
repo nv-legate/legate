nv-legate/legate

commit 9f1280a051bcb30b2a8ac07d8c742669cc3aed3f

Fix Many Build Warnings (#30)
* Add more public API test in machine unit test

* Revert "Add more public API test in machine unit test"

This reverts commit 1e2df63b79989810042ec4d610bbd2e9ff8c13c8.

* Add LEGATE_CPP_VERSION to specify current C++ version

* src/legate_defines.h: define macro

* Convert throw() to noexcept

* src/core/task/exception.h: fix
* src/core/data/detail/transform.h: fix

* Fix inconsistent struct vs class declarations to work around MS ABI incompatibilities

* src/core/data/array.h: class -> struct to match decl
* src/core/data/detail/logical_array.h: class -> struct to match decl
* src/core/data/detail/logical_region_field.h: class -> struct to match decl
...
* src/core/runtime/detail/runtime.h: class -> struct to match decl

* Fix pessimizing std::move on return value

* src/core/data/detail/logical_store.cc: remove
* src/core/data/detail/transform.cc: remove
* src/core/data/scalar.cc: remove from temporary
* src/core/mapping/array.cc: remove
* src/core/mapping/mapping.cc: remove
* src/core/mapping/operation.cc: remove
* src/core/operation/detail/operation.cc: remove
* src/core/task/task_context.cc: remove
* src/core/utilities/deserializer.cc: remove
* src/core/utilities/deserializer.inl: remove

* Add missing override specificier for virtual overriding functions

* src/core/partitioning/detail/constraint.h: add to validate()

* Fix constructor initialization order

* src/core/data/detail/logical_store.cc: initialize volume_ first
* src/core/data/detail/store.cc: initialize num_elements_ first
* src/core/mapping/detail/operation.cc: fix
* src/core/operation/detail/operation.cc: fix
* src/core/runtime/detail/projection.cc: fix

* Remove dead code

* src/core/data/detail/logical_store.cc: remove p_tiling
* src/core/data/detail/transform.cc: fix
* src/core/operation/detail/copy_launcher.cc: fix
* src/core/partitioning/detail/constraint.cc: fix

src/core/utilities/deserializer.cc
@@ -42,7 +42,7 @@ std::vector<std::shared_ptr<detail::Array>> TaskDeserializer::unpack_arrays()
   auto size = unpack<uint32_t>();
   arrays.reserve(size);
   for (uint32_t idx = 0; idx < size; ++idx) { arrays.emplace_back(unpack_array()); }
-  return std::move(arrays);
+  return arrays;
 }
 
 std::shared_ptr<detail::Array> TaskDeserializer::unpack_array()
@@ -140,7 +140,6 @@ void TaskDeserializer::_unpack(detail::RegionField& value)
 
 void TaskDeserializer::_unpack(detail::UnboundRegionField& value)
 {
-  auto dim = unpack<int32_t>();
   auto idx = unpack<uint32_t>();
   auto fid = unpack<int32_t>();
 
@@ -185,7 +184,7 @@ std::vector<std::shared_ptr<detail::Array>> TaskDeserializer::unpack_arrays()
   auto size = unpack<uint32_t>();
   arrays.reserve(size);
   for (uint32_t idx = 0; idx < size; ++idx) { arrays.emplace_back(unpack_array()); }
-  return std::move(arrays);
+  return arrays;
 }
 
 std::shared_ptr<detail::Array> TaskDeserializer::unpack_array()

src/core/comm/local_comm.cc
@@ -129,8 +129,6 @@ int LocalNetwork::alltoallv(const void* sendbuf,
                             CollDataType type,
                             CollComm global_comm)
 {
-  int res;
-
   int total_size  = global_comm->global_comm_size;
   int global_rank = global_comm->global_rank;
 
@@ -167,11 +165,11 @@ int LocalNetwork::alltoallv(const void* sendbuf,
       recvfrom_global_rank,
       recvfrom_seg_id,
       sdispls[recvfrom_seg_id],
-      src,
+      static_cast<void*>(src),
       global_rank,
       recvfrom_global_rank,
       rdispls[recvfrom_global_rank],
-      dst);
+      static_cast<void*>(dst));
 #endif
     memcpy(dst, src, recvcounts[recvfrom_global_rank] * type_extent);
   }
@@ -189,8 +187,6 @@ int LocalNetwork::alltoallv(const void* sendbuf,
 int LocalNetwork::alltoall(
   const void* sendbuf, void* recvbuf, int count, CollDataType type, CollComm global_comm)
 {
-  int res;
-
   int total_size  = global_comm->global_comm_size;
   int global_rank = global_comm->global_rank;
 
@@ -221,10 +217,10 @@ int LocalNetwork::alltoall(
       type_extent,
       recvfrom_global_rank,
       recvfrom_seg_id,
-      src,
+      static_cast<void*>(src),
       global_rank,
       recvfrom_global_rank,
-      dst);
+      static_cast<void*>(dst));
 #endif
     memcpy(dst, src, count * type_extent);
   }
@@ -271,7 +267,7 @@ int LocalNetwork::allgather(
       recvfrom_global_rank,
       src,
       global_rank,
-      dst);
+      static_cast<void*>(dst));
 #endif
     memcpy(dst, src, count * type_extent);
   }

src/core/data/array.h
@@ -23,7 +23,7 @@
 namespace legate {
 
 namespace detail {
-class Array;
+struct Array;
 }  // namespace detail
 
 class ListArray;

src/core/data/detail/logical_array.h
@@ -18,10 +18,10 @@
 
 namespace legate::detail {
 
-class Array;
+struct Array;
 class AutoTask;
 class BaseArray;
-class ConstraintSolver;
+struct ConstraintSolver;
 class ListLogicalArray;
 class Variable;
 

src/core/data/detail/logical_region_field.h
@@ -22,7 +22,7 @@
 #include "core/runtime/detail/field_manager.h"
 
 namespace legate {
-class Partition;
+struct Partition;
 class Tiling;
 }  // namespace legate
 

src/core/data/detail/logical_store.cc
@@ -51,8 +51,8 @@ Storage::Storage(const Shape& extents, std::shared_ptr<Type> type, bool optimize
   : storage_id_(Runtime::get_runtime()->get_unique_storage_id()),
     dim_(extents.size()),
     extents_(extents),
-    type_(std::move(type)),
     volume_(extents.volume()),
+    type_(std::move(type)),
     offsets_(dim_, 0)
 {
   if (optimize_scalar && volume_ == 1) kind_ = Kind::FUTURE;
@@ -65,10 +65,10 @@ Storage::Storage(const Shape& extents, std::shared_ptr<Type> type, const Legion:
   : storage_id_(Runtime::get_runtime()->get_unique_storage_id()),
     dim_(extents.size()),
     extents_(extents),
+    volume_(extents.volume()),
     type_(std::move(type)),
     kind_(Kind::FUTURE),
     future_(future),
-    volume_(extents.volume()),
     offsets_(dim_, 0)
 {
 #ifdef DEBUG_LEGATE
@@ -84,8 +84,8 @@ Storage::Storage(Shape&& extents,
   : storage_id_(Runtime::get_runtime()->get_unique_storage_id()),
     dim_(extents.size()),
     extents_(std::move(extents)),
-    type_(std::move(type)),
     volume_(extents_.volume()),
+    type_(std::move(type)),
     level_(parent->level() + 1),
     parent_(std::move(parent)),
     color_(std::move(color)),
@@ -140,7 +140,6 @@ std::shared_ptr<Storage> Storage::slice(Shape tile_shape, Shape offsets)
 
   auto tiling =
     create_tiling(std::move(tile_shape), std::move(color_shape), std::move(signed_offsets));
-  auto* p_tiling         = static_cast<const Tiling*>(tiling.get());
   auto storage_partition = root->create_partition(std::move(tiling), can_tile_completely);
   return storage_partition->get_child_storage(color);
 }
@@ -639,7 +638,7 @@ std::shared_ptr<Partition> LogicalStore::find_or_create_key_partition(
 #ifdef DEBUG_LEGATE
   assert(store_part != nullptr);
 #endif
-  return std::move(store_part);
+  return store_part;
 }
 
 bool LogicalStore::has_key_partition(const mapping::detail::Machine& machine,

src/core/data/detail/logical_store.h
@@ -26,9 +26,9 @@
 
 namespace legate::detail {
 
-class Analyzable;
+struct Analyzable;
 class LogicalStorePartition;
-class ProjectionInfo;
+struct ProjectionInfo;
 class Strategy;
 class StoragePartition;
 class Store;

src/core/data/detail/store.cc
@@ -76,15 +76,15 @@ Domain RegionField::domain() const { return dim_dispatch(dim_, get_domain_fn{}, 
 void RegionField::unmap() { detail::Runtime::get_runtime()->unmap_physical_region(pr_); }
 
 UnboundRegionField::UnboundRegionField(const Legion::OutputRegion& out, Legion::FieldID fid)
-  : out_(out),
-    fid_(fid),
-    num_elements_(
-      Legion::UntypedDeferredValue(sizeof(size_t), find_memory_kind_for_executing_processor()))
+  : num_elements_(
+      Legion::UntypedDeferredValue(sizeof(size_t), find_memory_kind_for_executing_processor())),
+    out_(out),
+    fid_(fid)
 {
 }
 
 UnboundRegionField::UnboundRegionField(UnboundRegionField&& other) noexcept
-  : bound_(other.bound_), out_(other.out_), fid_(other.fid_), num_elements_(other.num_elements_)
+  : bound_(other.bound_), num_elements_(other.num_elements_), out_(other.out_), fid_(other.fid_)
 {
   other.bound_        = false;
   other.out_          = Legion::OutputRegion();

commit 649be4cc5490d6b285ccbe0669f7d20f0b80c279

Fix a bug introduced by #30 (#42)

src/core/utilities/deserializer.cc
@@ -140,6 +140,7 @@ void TaskDeserializer::_unpack(detail::RegionField& value)
 
 void TaskDeserializer::_unpack(detail::UnboundRegionField& value)
 {
+  unpack<int32_t>();  // dim
   auto idx = unpack<uint32_t>();
   auto fid = unpack<int32_t>();
 