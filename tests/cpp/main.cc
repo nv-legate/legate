47120a14cd454e5d948fd3c5ba6a29309c4dea8c
Jacob Faibussowitsch <jacob.fai@gmail.com>
2023-10-18 17:08:35 -0400

    Embed default ASAN and LSAN options in test executable (#150)

--- tests/cpp/main.cc
+++ tests/cpp/main.cc
@@ -14,6 +14,27 @@
 #include "legate.h"
 #include "utilities/utilities.h"
 
+extern "C" {
+
+const char* __asan_default_options()  // NOLINT(bugprone-reserved-identifier)
+{
+  return "check_initialization_order=1:"
+         "detect_stack_use_after_return=1:"
+         "alloc_dealloc_mismatch=1:"
+         "strict_string_checks=1:"
+         "color=always:"
+         // note trailing ":", this is so that user may write ASAN_OPTIONS+="foo:bar:baz"
+         "symbolize=1:";
+}
+
+const char* __lsan_default_suppressions()  // NOLINT(bugprone-reserved-identifier)
+{
+  return "leak:librealm.*\n"
+         "leak:liblegion.*\n";
+}
+//
+}
+
 int main(int argc, char** argv)
 {
   ::testing::InitGoogleTest(&argc, argv);
