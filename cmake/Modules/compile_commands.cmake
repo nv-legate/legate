9e632ffd3175982e64336a2bbdc1ad11c01832d5
Jacob Faibussowitsch <jacob.fai@gmail.com>
2023-11-16 13:34:12 -0500

    Make compile_commands linking more robust in the face of add_subdirectory() (#251)

--- cmake/Modules/compile_commands.cmake
+++ cmake/Modules/compile_commands.cmake
@@ -21,7 +21,11 @@ macro(legate_core_export_compile_commands)
     message("-- Symlinking compile_commands.json to root directory")
     file(
       CREATE_LINK
-      ${CMAKE_CURRENT_BINARY_DIR}/compile_commands.json
+      # Using CMAKE_BINARY_DIR vs CMAKE_CURRENT_BINARY_DIR is intentional. If we use
+      # CURRENT_BINARY_DIR, then it breaks anytime someone uses add_subdirectory(), since
+      # cmake always puts the compile_commands.json in the top-level build dir (not in the
+      # sub tree)
+      ${CMAKE_BINARY_DIR}/compile_commands.json
       ${CMAKE_CURRENT_LIST_DIR}/compile_commands.json
       SYMBOLIC
     )
