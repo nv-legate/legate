nv-legate/legate

commit 0df8caba2dce6d7319b99a334a515c73adbb0951

Add implicit conversion for arithmetic scalars to add_scalar_arg (#261)
Add implicit conversion for arithmetic scalars to add_scalar_arg

src/core/operation/task.h
@@ -21,6 +21,7 @@
 #include "core/utilities/shared_ptr.h"
 
 #include <string>
+#include <type_traits>
 
 /**
  * @file
@@ -151,6 +152,17 @@ class AutoTask {
    * @param scalar A scalar to add to the task
    */
   void add_scalar_arg(Scalar&& scalar);
+
+  /**
+   @brief Template wrapper for arithmetic types to make adding scalar args
+          less verbose
+
+   @tparam T The arithmetic type
+   @param t The arithmetic scalar to convert to Scalar
+   */
+  template <class T, typename = std::enable_if_t<std::is_constructible<Scalar, T>::value>>
+  void add_scalar_arg(T&& t);
+
   /**
    * @brief Adds a partitioning constraint to the task
    *
@@ -317,6 +329,16 @@ class ManualTask {
   void add_scalar_arg(Scalar&& scalar);
 
   /**
+   @brief Template wrapper for arithmetic types to make adding scalar args
+          less verbose
+
+   @tparam T The arithmetic type
+   @param t The arithmetic scalar to convert to Scalar
+   */
+  template <class T, typename = std::enable_if_t<std::is_constructible<Scalar, T>::value>>
+  void add_scalar_arg(T&& t);
+
+  /**
    * @brief Returns the provenance information of this operation
    *
    * @return Provenance
@@ -368,3 +390,5 @@ class ManualTask {
 };
 
 }  // namespace legate
+
+#include "core/operation/task.inl"

legate_core_cpp.cmake
@@ -529,6 +529,7 @@ install(
   FILES src/core/operation/projection.h
         src/core/operation/projection.inl
         src/core/operation/task.h
+        src/core/operation/task.inl
   DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/legate/core/operation)
 
 install(

src/core/operation/task.inl
@@ -1,0 +1,31 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-License-Identifier: LicenseRef-NvidiaProprietary
+ *
+ * NVIDIA CORPORATION, its affiliates and licensors retain all intellectual
+ * property and proprietary rights in and to this material, related
+ * documentation and any modifications thereto. Any use, reproduction,
+ * disclosure or distribution of this material and related documentation
+ * without an express license agreement from NVIDIA CORPORATION or
+ * its affiliates is strictly prohibited.
+ */
+
+#pragma once
+
+#include "core/operation/task.h"
+
+namespace legate {
+
+template <class T, typename Enable>
+void AutoTask::add_scalar_arg(T&& t)
+{
+  add_scalar_arg(Scalar{std::forward<T>(t)});
+}
+
+template <class T, typename Enable>
+void ManualTask::add_scalar_arg(T&& t)
+{
+  add_scalar_arg(Scalar{std::forward<T>(t)});
+}
+
+}  // namespace legate

commit e5252577709603195ba7fa31e8f94d10c6cdf3a4

Several minor bug fixes (#335)
* Apply the same naming convention to legate::tuple

* Direct object constructions should not crash

* Runtime.create_scalar is obsolete now

* Mark writable future-backed stores reducible as well

* Stop leaking instance managers

* Return value from InstanceManager::record_instance never gets used

* Fix clang-tidy errors introduced by #261

legate/core/_lib/data/logical_array.pyx
@@ -29,6 +29,11 @@ cdef class LogicalArray:
         result._handle = handle
         return result
 
+    def __init__(self) -> None:
+        raise ValueError(
+            f"{type(self).__name__} objects must not be constructed directly"
+        )
+
     @staticmethod
     def from_store(LogicalStore store) -> LogicalArray:
         return LogicalArray.from_handle(_LogicalArray(store._handle))

legate/core/_lib/data/logical_store.pyx
@@ -32,6 +32,11 @@ cdef class LogicalStore:
         handle.impl().get().allow_out_of_order_destruction()
         return result
 
+    def __init__(self) -> None:
+        raise ValueError(
+            f"{type(self).__name__} objects must not be constructed directly"
+        )
+
     @property
     def shape(self) -> Shape:
         return Shape(self.extents)

legate/core/_lib/data/physical_array.pyx
@@ -18,5 +18,10 @@ cdef class PhysicalArray:
         result._handle = array
         return result
 
+    def __init__(self) -> None:
+        raise ValueError(
+            f"{type(self).__name__} objects must not be constructed directly"
+        )
+
     cpdef PhysicalStore data(self):
         return PhysicalStore.from_handle(self._handle.data())

legate/core/_lib/data/physical_store.pyx
@@ -24,6 +24,11 @@ cdef class PhysicalStore:
         result._handle = handle
         return result
 
+    def __init__(self) -> None:
+        raise ValueError(
+            f"{type(self).__name__} objects must not be constructed directly"
+        )
+
     @property
     def ndim(self) -> int32_t:
         return self._handle.dim()

legate/core/_lib/operation/projection.pxd
@@ -13,7 +13,7 @@ from libc.stdint cimport int32_t
 from libcpp cimport bool
 from libcpp.string cimport string as std_string
 
-from ..utilities.tuple cimport tuple as _tuple
+from ..utilities.tuple cimport _tuple
 
 
 cdef extern from "core/operation/projection.h" namespace "legate" nogil:

legate/core/_lib/operation/projection.pyx
@@ -20,6 +20,11 @@ cdef class SymbolicExpr:
         result._handle = std_move(handle)
         return result
 
+    def __init__(self) -> None:
+        raise ValueError(
+            f"{type(self).__name__} objects must not be constructed directly"
+        )
+
     @property
     def dim(self) -> int:
         return self._handle.dim()

legate/core/_lib/operation/task.pxd
@@ -17,7 +17,7 @@ from ..data.logical_array cimport _LogicalArray
 from ..data.logical_store cimport _LogicalStore, _LogicalStorePartition
 from ..data.scalar cimport _Scalar
 from ..partitioning.constraint cimport _Constraint, _Variable
-from ..utilities.tuple cimport tuple as _tuple
+from ..utilities.tuple cimport _tuple
 from .projection cimport _SymbolicPoint
 
 

legate/core/_lib/operation/task.pyx
@@ -28,7 +28,7 @@ from .projection cimport SymbolicExpr, _SymbolicPoint
 
 from ..type.type_info import Type, array_type, null_type
 
-from ..utilities.tuple cimport tuple as _tuple
+from ..utilities.tuple cimport _tuple
 
 from ...utils import is_iterable
 
@@ -57,6 +57,11 @@ cdef class AutoTask:
         result._handle = std_move(handle)
         result._exception_types = []
         return result
+
+    def __init__(self) -> None:
+        raise ValueError(
+            f"{type(self).__name__} objects must not be constructed directly"
+        )
 
     def add_input(
         self, array_or_store, partition: Union[Variable, None] = None
@@ -310,6 +315,11 @@ cdef class ManualTask:
         result._handle = std_move(handle)
         result._exception_types = []
         return result
+
+    def __init__(self) -> None:
+        raise ValueError(
+            f"{type(self).__name__} objects must not be constructed directly"
+        )
 
     def add_input(
         self,

legate/core/_lib/partitioning/constraint.pxd
@@ -13,7 +13,7 @@ from libc.stdint cimport int32_t
 from libcpp.string cimport string as std_string
 
 from ..data.shape cimport _Shape
-from ..utilities.tuple cimport tuple as _tuple
+from ..utilities.tuple cimport _tuple
 
 
 cdef extern from "core/partitioning/constraint.h" namespace "legate" nogil:

legate/core/_lib/partitioning/constraint.pyx
@@ -13,7 +13,7 @@ from libc.stdint cimport int32_t
 from libcpp.utility cimport move as std_move
 
 from ..data.shape cimport _Shape
-from ..utilities.tuple cimport tuple as _tuple
+from ..utilities.tuple cimport _tuple
 from ...utils import is_iterable
 
 
@@ -23,6 +23,11 @@ cdef class Variable:
         cdef Variable result = Variable.__new__(Variable)
         result._handle = handle
         return result
+
+    def __init__(self) -> None:
+        raise ValueError(
+            f"{type(self).__name__} objects must not be constructed directly"
+        )
 
     def __str__(self) -> str:
         return self._handle.to_string().decode()
@@ -37,6 +42,11 @@ cdef class Constraint:
         cdef Constraint result = Constraint.__new__(Constraint)
         result._handle = handle
         return result
+
+    def __init__(self) -> None:
+        raise ValueError(
+            f"{type(self).__name__} objects must not be constructed directly"
+        )
 
     def __str__(self) -> str:
         return self._handle.to_string().decode()

legate/core/_lib/runtime/library.pyx
@@ -23,6 +23,11 @@ cdef class Library:
         result._handle = handle
         return result
 
+    def __init__(self) -> None:
+        raise ValueError(
+            f"{type(self).__name__} objects must not be constructed directly"
+        )
+
     cpdef int64_t get_new_task_id(self):
         return self._handle.get_new_task_id()
 

legate/core/_lib/runtime/runtime.pyx
@@ -88,6 +88,11 @@ cdef class Runtime:
         result._handle = handle
         return result
 
+    def __init__(self) -> None:
+        raise ValueError(
+            f"{type(self).__name__} objects must not be constructed directly"
+        )
+
     def find_library(self, str library_name) -> Library:
         return Library.from_handle(
             self._handle.find_library(library_name.encode())
@@ -518,26 +523,6 @@ cdef class Runtime:
                 std_move(cpp_shape), dtype._handle, std_move(alloc)
             )
         )
-
-    def create_scalar(self, Type dtype, data: Any) -> Scalar:
-        """
-        Creates a future from a buffer holding a scalar value. The value is
-        copied to the future.
-
-        Parameters
-        ----------
-        data : buffer
-            Buffer that holds a scalar value
-
-        size : int
-            Size of the value
-
-        Returns
-        -------
-        Future
-            A new future
-        """
-        return Scalar.create_from_buffer(data, dtype)
 
     def issue_execution_fence(self, bool block = False) -> None:
         with cython.nogil:

legate/core/_lib/task/task_context.pyx
@@ -29,6 +29,11 @@ cdef class TaskContext:
         result._scalars = None
         return result
 
+    def __init__(self) -> None:
+        raise ValueError(
+            f"{type(self).__name__} objects must not be constructed directly"
+        )
+
     cpdef int64_t get_task_id(self):
         return self._handle.task_id()
 

legate/core/_lib/task/task_info.pyx
@@ -126,6 +126,11 @@ cdef class TaskInfo:
         result._registered_variants = {}
         return result
 
+    def __init__(self) -> None:
+        raise ValueError(
+            f"{type(self).__name__} objects must not be constructed directly"
+        )
+
     cdef _TaskInfo *release(self) except NULL:
         if not self.valid:
             raise RuntimeError(

legate/core/_lib/type/type_info.pyx
@@ -66,6 +66,9 @@ cdef class Type:
         result._handle = ty
         return result
 
+    def __init__(self) -> None:
+        self._handle = _null_type()
+
     @property
     def code(self) -> int32_t:
         return <int32_t> self._handle.code()
@@ -120,6 +123,11 @@ cdef class Type:
 
 
 cdef class FixedArrayType(Type):
+    def __init__(self) -> None:
+        raise ValueError(
+            f"{type(self).__name__} objects must not be constructed directly"
+        )
+
     @property
     def num_elements(self) -> uint32_t:
         return self._handle.as_fixed_array_type().num_elements()
@@ -137,6 +145,11 @@ cdef class FixedArrayType(Type):
 
 
 cdef class StructType(Type):
+    def __init__(self) -> None:
+        raise ValueError(
+            f"{type(self).__name__} objects must not be constructed directly"
+        )
+
     @property
     def num_fields(self) -> uint32_t:
         return self._handle.as_struct_type().num_fields()

legate/core/_lib/utilities/tuple.pxd
@@ -14,7 +14,7 @@ from libcpp.vector cimport vector as std_vector
 
 
 cdef extern from "core/utilities/tuple.h" namespace "legate" nogil:
-    cdef cppclass tuple[T]:
+    cdef cppclass _tuple "legate::tuple" [T]:
         void append_inplace(const T& value)
         const std_vector[T]& data() const
         void reserve(size_t)

src/core/data/detail/physical_store.inl
@@ -84,7 +84,8 @@ inline PhysicalStore::PhysicalStore(int32_t dim,
     future_{std::move(future)},
     transform_{std::move(transform)},
     readable_{future_.valid()},
-    writable_{!future_.is_read_only()}
+    writable_{!future_.is_read_only()},
+    reducible_{writable_}
 {
 }
 

src/core/mapping/detail/base_mapper.cc
@@ -813,7 +813,7 @@ bool BaseMapper::map_legate_store(Legion::Mapping::MapperContext ctx,
     if (!result.is_external_instance() && group != nullptr) {
       assert(fields.size() == 1);
       auto fid = fields.front();
-      static_cast<void>(local_instances->record_instance(group, fid, result, policy));
+      local_instances->record_instance(group, fid, result, policy);
     }
     runtime->enable_reentrant(ctx);
     // We made it so no need for an acquire

src/core/mapping/detail/instance_manager.cc
@@ -398,14 +398,13 @@ RegionGroupP InstanceManager::find_region_group(const Region& region,
   return result;
 }
 
-std::set<InstanceManager::Instance> InstanceManager::record_instance(
-  const RegionGroupP& group,
-  FieldID field_id,
-  const Instance& instance,
-  const InstanceMappingPolicy& policy)
+void InstanceManager::record_instance(const RegionGroupP& group,
+                                      FieldID field_id,
+                                      const Instance& instance,
+                                      const InstanceMappingPolicy& policy)
 {
   FieldMemInfo key{instance.get_tree_id(), field_id, instance.get_location()};
-  return instance_sets_[std::move(key)].record_instance(group, instance, policy);
+  static_cast<void>(instance_sets_[std::move(key)].record_instance(group, instance, policy));
 }
 
 void InstanceManager::erase(const Instance& inst)
@@ -442,12 +441,8 @@ std::map<Memory, size_t> InstanceManager::aggregate_instance_sizes() const
 
 /*static*/ InstanceManager* InstanceManager::get_instance_manager()
 {
-  static InstanceManager* manager{nullptr};
-
-  if (nullptr == manager) {
-    manager = new InstanceManager{};
-  }
-  return manager;
+  static auto manager = std::make_unique<InstanceManager>();
+  return manager.get();
 }
 
 bool ReductionInstanceManager::find_instance(ReductionOpID& redop,
@@ -492,12 +487,8 @@ void ReductionInstanceManager::erase(const Instance& inst)
 
 /*static*/ ReductionInstanceManager* ReductionInstanceManager::get_instance_manager()
 {
-  static ReductionInstanceManager* manager{nullptr};
-
-  if (manager == nullptr) {
-    manager = new ReductionInstanceManager{};
-  }
-  return manager;
+  static auto manager = std::make_unique<ReductionInstanceManager>();
+  return manager.get();
 }
 
 }  // namespace legate::mapping::detail

src/core/mapping/detail/instance_manager.h
@@ -165,12 +165,14 @@ class InstanceManager : public BaseInstanceManager {
                                                FieldID field_id,
                                                Memory memory,
                                                bool exact = false);
-  [[nodiscard]] std::set<Instance> record_instance(const RegionGroupP& group,
-                                                   FieldID field_id,
-                                                   const Instance& instance,
-                                                   const InstanceMappingPolicy& policy = {});
+  void record_instance(const RegionGroupP& group,
+                       FieldID field_id,
+                       const Instance& instance,
+                       const InstanceMappingPolicy& policy = {});
 
   void erase(const Instance& inst);
+
+  void destroy();
 
   [[nodiscard]] static InstanceManager* get_instance_manager();
 
@@ -198,6 +200,8 @@ class ReductionInstanceManager : public BaseInstanceManager {
                        const InstanceMappingPolicy& policy = {});
 
   void erase(const Instance& inst);
+
+  void destroy();
 
   [[nodiscard]] static ReductionInstanceManager* get_instance_manager();
 

src/core/mapping/detail/instance_manager.inl
@@ -68,4 +68,12 @@ inline size_t BaseInstanceManager::FieldMemInfo::hash() const noexcept
 
 inline Legion::Mapping::LocalLock& BaseInstanceManager::manager_lock() { return manager_lock_; }
 
+// ==========================================================================================
+
+inline void InstanceManager::destroy() { instance_sets_.clear(); }
+
+// ==========================================================================================
+
+inline void ReductionInstanceManager::destroy() { instance_sets_.clear(); }
+
 }  // namespace legate::mapping::detail

src/core/operation/task.h
@@ -160,7 +160,7 @@ class AutoTask {
    @tparam T The arithmetic type
    @param t The arithmetic scalar to convert to Scalar
    */
-  template <class T, typename = std::enable_if_t<std::is_constructible<Scalar, T>::value>>
+  template <class T, typename = std::enable_if_t<std::is_constructible_v<Scalar, T>>>
   void add_scalar_arg(T&& t);
 
   /**
@@ -335,7 +335,7 @@ class ManualTask {
    @tparam T The arithmetic type
    @param t The arithmetic scalar to convert to Scalar
    */
-  template <class T, typename = std::enable_if_t<std::is_constructible<Scalar, T>::value>>
+  template <class T, typename = std::enable_if_t<std::is_constructible_v<Scalar, T>>>
   void add_scalar_arg(T&& t);
 
   /**

src/core/runtime/detail/runtime.cc
@@ -20,6 +20,7 @@
 #include "core/data/detail/logical_store.h"
 #include "core/mapping/detail/core_mapper.h"
 #include "core/mapping/detail/default_mapper.h"
+#include "core/mapping/detail/instance_manager.h"
 #include "core/mapping/detail/machine.h"
 #include "core/mapping/detail/mapping.h"
 #include "core/operation/detail/copy.h"
@@ -1348,6 +1349,9 @@ void Runtime::destroy()
 
   // We're about to deallocate objects below, so let's block on all outstanding Legion operations
   issue_execution_fence(true);
+
+  mapping::detail::InstanceManager::get_instance_manager()->destroy();
+  mapping::detail::ReductionInstanceManager::get_instance_manager()->destroy();
 
   // Any STL containers holding Legion handles need to be cleared here, otherwise they cause
   // trouble when they get destroyed in the Legate runtime's destructor