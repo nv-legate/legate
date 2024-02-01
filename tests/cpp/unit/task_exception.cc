9b39d09ade168cc0a0ced47193a76accc4f84243
Robin Wang <104830875+robinwnv@users.noreply.github.com>
2024-02-01 09:23:57 +0800

    Add tests for task module. (#333)
    * Add tests for task module.
    
    * Address comments.
    
    * Address comments - part2
    
    * Update test code to catch up with legate changes.

---
+++ tests/cpp/unit/task_exception.cc
@@ -0,0 +1,36 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-License-Identifier: LicenseRef-NvidiaProprietary
+ *
+ * NVIDIA CORPORATION, its affiliates and licensors retain all intellectual
+ * property and proprietary rights in and to this material, related
+ * documentation and any modifications thereto. Any use, reproduction,
+ * disclosure or distribution of this material and related documentation
+ * without an express license agreement from NVIDIA CORPORATION or
+ * its affiliates is strictly prohibited.
+ */
+
+#include "legate.h"
+#include "utilities/utilities.h"
+
+#include <gtest/gtest.h>
+
+namespace task_exception_test {
+
+using TaskException = DefaultFixture;
+
+TEST_F(TaskException, Basic)
+{
+  int32_t index1         = 100;
+  const char* ERROR_MSG1 = "Exception Test1";
+  legate::TaskException exc1{index1, ERROR_MSG1};
+  EXPECT_EQ(exc1.index(), index1);
+  EXPECT_STREQ(exc1.error_message().c_str(), ERROR_MSG1);
+
+  const char* ERROR_MSG2 = "Exception Test2";
+  legate::TaskException exc2{ERROR_MSG2};
+  EXPECT_EQ(exc2.index(), 0);
+  EXPECT_STREQ(exc2.error_message().c_str(), ERROR_MSG2);
+}
+
+}  // namespace task_exception_test
---
+++ tests/cpp/unit/variant_options.cc
@@ -0,0 +1,47 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-License-Identifier: LicenseRef-NvidiaProprietary
+ *
+ * NVIDIA CORPORATION, its affiliates and licensors retain all intellectual
+ * property and proprietary rights in and to this material, related
+ * documentation and any modifications thereto. Any use, reproduction,
+ * disclosure or distribution of this material and related documentation
+ * without an express license agreement from NVIDIA CORPORATION or
+ * its affiliates is strictly prohibited.
+ */
+
+#include "legate.h"
+#include "utilities/utilities.h"
+
+#include <gtest/gtest.h>
+
+namespace variant_options_test {
+
+using VariantOptions = DefaultFixture;
+
+TEST_F(VariantOptions, Basic)
+{
+  legate::VariantOptions options;
+  EXPECT_EQ(options.leaf, true);
+  EXPECT_EQ(options.inner, false);
+  EXPECT_EQ(options.idempotent, false);
+  EXPECT_EQ(options.concurrent, false);
+  EXPECT_EQ(options.return_size, legate::LEGATE_MAX_SIZE_SCALAR_RETURN);
+
+  options.with_leaf(false);
+  EXPECT_EQ(options.leaf, false);
+
+  options.with_inner(true);
+  EXPECT_EQ(options.inner, true);
+
+  options.with_idempotent(true);
+  EXPECT_EQ(options.idempotent, true);
+
+  options.with_concurrent(true);
+  EXPECT_EQ(options.concurrent, true);
+
+  options.with_return_size(256);
+  EXPECT_EQ(options.return_size, 256);
+}
+
+}  // namespace variant_options_test
