c42bc470dd2ce5dd8cd8161562660cfd8d7f21e9
XiaLuNV <110973296+XiaLuNV@users.noreply.github.com>
2024-03-05 10:41:59 +0800

    add logicalarray test cases (#445)
    * add logicalarray test case
    
    * add logicalarray test case
    
    * add logicalarray test case
    
    * add logicalarray test case

--- tests/unit/legate/core/test_type_uid.py
+++ tests/unit/legate/core/test_type_uid.py
@@ -15,22 +15,7 @@ import pytest
 
 import legate.core.types as ty
 
-_PRIMITIVES = [
-    ty.bool_,
-    ty.int8,
-    ty.int16,
-    ty.int32,
-    ty.int64,
-    ty.uint8,
-    ty.uint16,
-    ty.uint32,
-    ty.uint64,
-    ty.float16,
-    ty.float32,
-    ty.float64,
-    ty.complex64,
-    ty.complex128,
-]
+from .util.types import _PRIMITIVES
 
 _PRIMITIVES_UIDS = {type.uid for type in _PRIMITIVES}
 
