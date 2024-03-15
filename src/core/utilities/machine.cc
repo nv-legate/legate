5d42cb76232aa255cf6e274a888da8950102fd12
Wonchan Lee <wonchanl@nvidia.com>
2024-03-15 11:08:36 -0700

    Handle Python processor kind correctly in the memory query (#484)

--- src/core/utilities/machine.cc
+++ src/core/utilities/machine.cc
@@ -24,6 +24,7 @@ Memory::Kind find_memory_kind_for_executing_processor(bool host_accessible)
       return host_accessible ? Memory::Kind::Z_COPY_MEM : Memory::Kind::GPU_FB_MEM;
     case Processor::Kind::OMP_PROC:
       return detail::Config::has_socket_mem ? Memory::Kind::SOCKET_MEM : Memory::Kind::SYSTEM_MEM;
+    case Processor::Kind::PY_PROC: return Memory::Kind::SYSTEM_MEM;
     default: LEGATE_ABORT("Unknown processor kind " << kind);
   }
   return Memory::Kind::SYSTEM_MEM;
