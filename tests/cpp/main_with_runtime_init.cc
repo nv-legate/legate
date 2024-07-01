3a3fb8fa8721c2ba50766d067e94aed26113e0cf
Jacob Faibussowitsch <jacob.fai@gmail.com>
2024-07-01 11:53:49 -0400

    Extract the sanitizer default options into a header (#883)

--- tests/cpp/main_with_runtime_init.cc
+++ tests/cpp/main_with_runtime_init.cc
@@ -12,52 +12,10 @@
 
 #include "core/experimental/stl/detail/registrar.hpp"
 #include "legate.h"
+#include "utilities/sanitizer_options.h"
 #include "utilities/utilities.h"
 
 #include <gtest/gtest.h>
-
-extern "C" {
-
-// NOLINTBEGIN
-const char* __asan_default_options()
-{
-  return "check_initialization_order=1:"
-         "detect_stack_use_after_return=1:"
-         "alloc_dealloc_mismatch=1:"
-         "strict_string_checks=1:"
-         "color=always:"
-         "detect_odr_violation=2:"
-         "abort_on_error=1:"
-#if LEGATE_DEFINED(LEGATE_USE_CUDA)
-         "protect_shadow_gap=0:"
-#endif
-         // note trailing ":", this is so that user may write ASAN_OPTIONS+="foo:bar:baz"
-         "symbolize=1:";
-}
-
-const char* __ubsan_default_options() { return "print_stacktrace=1:"; }
-
-const char* __lsan_default_suppressions()
-{
-  return "leak:librealm.*\n"
-         "leak:liblegion.*\n";
-}
-
-const char* __tsan_default_options()
-{
-  return "halt_on_error=1:"
-         "second_deadlock_stack=1:"
-         "symbolize=1:"
-         "detect_deadlocks=1:";
-}
-
-const char* __tsan_default_suppressions()
-{
-  return "race:Legion::Internal::MemoryManager::create_eager_instance\n"
-         "race:Legion::Internal::Operation::perform_registration\n";
-}
-// NOLINTEND
-}
 
 int main(int argc, char** argv)
 {
