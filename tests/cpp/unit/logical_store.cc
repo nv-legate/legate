f1d317b546b9764b9e5633e79e5841f846a56f94
Joy Shen <110752938+joyshennv@users.noreply.github.com>
2024-07-22 09:19:55 +0800

    Add more cases for physical store. (#962)
    * Add more cases for physical store.
    
    * Address comments
    
    * Add tests for logical store code coverage.
    
    * Address comments

--- tests/cpp/unit/logical_store.cc
+++ tests/cpp/unit/logical_store.cc
@@ -796,6 +796,21 @@ TEST_F(LogicalStoreUnit, EqualStorageFuture)
   EXPECT_TRUE(store.equal_storage(promoted));
 }
 
+TEST_F(LogicalStoreUnit, ToString)
+{
+  const auto runtime         = legate::Runtime::get_runtime();
+  const auto bound_store     = runtime->create_store(legate::Scalar{std::int32_t{10}});
+  const auto bound_store_str = "Store(1) {shape: [1], storage: 1}";
+  ASSERT_EQ(bound_store.to_string(), bound_store_str);
+  const auto promoted = bound_store.promote(0, 5);
+  const auto promoted_store_str =
+    "Store(2) {shape: [5, 1], transform: Promote(extra_dim: 0, dim_size: 5), storage: 1}";
+  ASSERT_EQ(promoted.to_string(), promoted_store_str);
+  const auto unbound_store     = runtime->create_store(legate::int64());
+  const auto unbound_store_str = "Store(3) {shape: (unbound), storage: 2}";
+  ASSERT_EQ(unbound_store.to_string(), unbound_store_str);
+}
+
 // NOLINTEND(readability-magic-numbers)
 
 }  // namespace logicalstore_unit
--- tests/cpp/unit/physical_store.cc
+++ tests/cpp/unit/physical_store.cc
@@ -1241,9 +1241,23 @@ TEST_F(PhysicalStoreUnit, ReadOnlyFutureStoreInlineAllocation)
   auto logical_store = runtime->create_store(legate::Scalar{UINT64_VALUE});
   auto store         = logical_store.get_physical_store();
   auto inline_alloc  = store.get_inline_allocation();
-  EXPECT_NE(inline_alloc.ptr, nullptr);
-  EXPECT_EQ(inline_alloc.strides.size(), store.dim());
-  EXPECT_EQ(inline_alloc.strides.at(0), 0);
+  ASSERT_NE(inline_alloc.ptr, nullptr);
+  ASSERT_EQ(inline_alloc.strides.size(), store.dim());
+  ASSERT_EQ(inline_alloc.strides.at(0), 0);
+}
+
+TEST_F(PhysicalStoreUnit, TransformedFutureStoreInlineAllocation)
+{
+  auto runtime       = legate::Runtime::get_runtime();
+  auto logical_store = runtime->create_store(legate::Scalar{UINT64_VALUE});
+  auto promoted      = logical_store.promote(0, 1);
+  auto store         = promoted.get_physical_store();
+  ASSERT_TRUE(store.transformed());
+
+  auto inline_alloc = store.get_inline_allocation();
+  ASSERT_NE(inline_alloc.ptr, nullptr);
+  ASSERT_EQ(inline_alloc.strides.size(), store.dim());
+  ASSERT_EQ(inline_alloc.strides.at(0), 0);
 }
 
 TEST_F(PhysicalStoreUnit, BoundStoreInlineAllocation)
@@ -1252,10 +1266,10 @@ TEST_F(PhysicalStoreUnit, BoundStoreInlineAllocation)
   auto logical_store = runtime->create_store({2, 3}, legate::uint32());
   auto store         = logical_store.get_physical_store();
   auto inline_alloc  = store.get_inline_allocation();
-  EXPECT_NE(inline_alloc.ptr, nullptr);
-  EXPECT_EQ(inline_alloc.strides.size(), store.dim());
-  EXPECT_EQ(inline_alloc.strides.at(0), sizeof(std::uint32_t) * 3);
-  EXPECT_EQ(inline_alloc.strides.at(1), sizeof(std::uint32_t));
+  ASSERT_NE(inline_alloc.ptr, nullptr);
+  ASSERT_EQ(inline_alloc.strides.size(), store.dim());
+  ASSERT_EQ(inline_alloc.strides.at(0), sizeof(std::uint32_t) * 3);
+  ASSERT_EQ(inline_alloc.strides.at(1), sizeof(std::uint32_t));
 }
 
 // NOLINTEND(readability-magic-numbers)
