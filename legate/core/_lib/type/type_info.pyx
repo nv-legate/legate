9cd184e4869822e915a83f653caba641d3499304
Jacob Faibussowitsch <jacob.fai@gmail.com>
2024-08-27 10:19:31 -0400

    Document type directory (#1145)
    * Document type directory

--- legate/core/_lib/type/type_info.pyx
+++ legate/core/_lib/type/type_info.pyx
@@ -146,44 +146,153 @@ cdef class Type:
         return result
 
     def __init__(self) -> None:
+        r"""
+        Construct a `Type` (by default a "null" type).
+        """
         self._handle = _null_type()
 
     @property
-    def code(self) -> _Type.Code:
-        return <_Type.Code>self._handle.code()
+    def code(self) -> TypeCode:
+        r"""
+        Get the type code of the type.
+
+        Returns
+        -------
+        TypeCode
+            The type code.
+        """
+        return self._handle.code()
 
     @property
     def size(self) -> uint32_t:
+        r"""
+        Get the size (in bytes) of the data type.
+
+        Returns
+        -------
+        int
+            The size of the data type.
+        """
         return self._handle.size()
 
     @property
     def alignment(self) -> uint32_t:
+        r"""
+        Get the alignmenent (in bytes) of the data type.
+
+        Returns
+        -------
+        int
+            The alignment of the data type.
+        """
         return self._handle.alignment()
 
     @property
     def uid(self) -> int32_t:
+        r"""
+        Get the UID (universal ID) of the type.
+
+        All types which share a UID are guaranteed to be equal.
+
+        Returns
+        -------
+        int
+            The UID of the type.
+        """
         return self._handle.uid()
 
     @property
     def variable_size(self) -> bool:
+        r"""
+        Get whether the data type is variably sized.
+
+        Returns
+        -------
+        bool
+            `True` if the type is variable size, `False` otherwise.
+        """
         return self._handle.variable_size()
 
     @property
     def is_primitive(self) -> bool:
+        r"""
+        Get whether a type is "primitive".
+
+        Primitive types are generally those that are "built in", such as
+        `int32_t`, `uin64_t`, `float`, `double`, `int`, etc. Struct types, list
+        types, and other such compound types are not primitive.
+
+        Returns
+        -------
+        bool
+            `True` if the type is primitive, `False` otherwise.
+        """
         return self._handle.is_primitive()
 
     cpdef void record_reduction_op(
         self, int32_t op_kind, _GlobalRedopID reduction_op_id
     ):
+        r"""
+        Record a reduction operator.
+
+        The global ID of the reduction operator is issued when that operator is
+        registered to the runtime.
+
+        Parameters
+        ----------
+        op_kind : ReductionOpKind | int
+            The reduction operator kind.
+        reduction_op_id : GlobalRedopID
+            The global reduction ID.
+        """
         self._handle.record_reduction_operator(op_kind, reduction_op_id)
 
     cpdef _GlobalRedopID reduction_op_id(self, int32_t op_kind):
+        r"""
+        Finds the global redop ID for a given reduction op kind.
+
+        Parameters
+        ----------
+        op_kind : ReductionOpKind | int
+            The redop kind.
+
+        Returns
+        -------
+        GlobalRedopID
+            The global redop ID.
+
+        Raises
+        ------
+        ValueError
+            If `op_kind` does not exist for this type.
+        """
         return self._handle.find_reduction_operator(op_kind)
 
     def __repr__(self) -> str:
+        r"""
+        Return a human-readable string representation of the type.
+
+        Returns
+        -------
+        str
+            The string representation.
+        """
         return self._handle.to_string().decode()
 
     cpdef object to_numpy_dtype(self):
+        r"""
+        Get the equivalent numpy `dtype` for this type.
+
+        Returns
+        -------
+        np.dtype
+            The numpy dtype.
+
+        Raises
+        ------
+        ValueError
+            If the equivalent numpy `dtype` could not be determined.
+        """
         cdef _Type.Code code = self.code
         try:
             return _TO_NUMPY_DTYPES[code]
@@ -192,13 +301,44 @@ cdef class Type:
 
     @property
     def raw_ptr(self) -> uintptr_t:
+        r"""
+        Get a raw pointer to the C++ `Type` object.
+
+        Returns
+        -------
+        int
+            The pointer to the C++ object.
+        """
         return <uintptr_t>(&self._handle)
 
     def __hash__(self) -> int:
-        return hash((self.__class__, self.code))
+        r"""
+        Compute the hash of the type.
 
-    def __eq__(self, Type other) -> bool:
-        return isinstance(other, Type) and self._handle == other._handle
+        Returns
+        -------
+        int
+            The hash.
+        """
+        return self.uid
+
+    def __eq__(self, object other) -> bool:
+        r"""
+        Compate this type against another.
+
+        Parameters
+        ----------
+        other : Any
+            The other to compare against.
+
+        Returns
+        -------
+        bool
+            `True` if this type equals `other`, `False` otherwise.
+        """
+        if isinstance(other, Type):
+            return self._handle == (<Type> other)._handle
+        return NotImplemented
 
     @staticmethod
     cdef Type from_py_object(object py_object):
@@ -229,15 +369,54 @@ cdef class FixedArrayType(Type):
 
     @property
     def num_elements(self) -> uint32_t:
+        r"""
+        Get the number of elements of this array.
+
+        Only gets the "top-level" number of elements. For example, if this type
+        refers to an array of arrays:
+        ```
+        FixedArrayType[
+          FixedArrayType[uint32_t, uint32_t, uint32_t, uint32_t],
+          FixedArrayType[uint32_t, uint32_t, uint32_t, uint32_t]
+        ]
+        ```
+        this function would return `2`, not `4`.
+
+        Returns
+        -------
+        int
+            The number of elements.
+        """
         return self._handle.as_fixed_array_type().num_elements()
 
     @property
     def element_type(self) -> Type:
+        r"""
+        Get the element type of the array type.
+
+        Returns
+        -------
+        Type
+            The element type.
+        """
         return Type.from_handle(
             self._handle.as_fixed_array_type().element_type()
         )
 
     cpdef object to_numpy_dtype(self):
+        r"""
+        Get the equivalent numpy `dtype` for this type.
+
+        Returns
+        -------
+        np.dtype
+            The numpy dtype.
+
+        Raises
+        ------
+        ValueError
+            If the equivalent numpy `dtype` could not be determined.
+        """
         elem_ty = self.element_type.to_numpy_dtype()
         cdef uint32_t N = self.num_elements
         return np.dtype((elem_ty, (N, ))) if N > 1 else elem_ty
@@ -253,22 +432,86 @@ cdef class StructType(Type):
 
     @property
     def num_fields(self) -> uint32_t:
+        r"""
+        Get the number of fields in the struct type.
+
+        Only gets the "top-level" number of fields. For example, if this type
+        refers to a struct of structs:
+        ```
+        StructType[
+          StructType[
+            uint32_t, float32, complex128, string_type
+          ]
+          StructType[
+            int16_t
+          ]
+        ]
+        ```
+        this function would return `2`, not `4` or `1`.
+
+        Returns
+        -------
+        int
+            The number of fields in the struct type.
+        """
         return self._handle.as_struct_type().num_fields()
 
     cpdef Type field_type(self, uint32_t field_idx):
+        r"""
+        Get the type of a particular field.
+
+        Parameters
+        ----------
+        field_idx : int
+            The index of the field to get.
+
+        Returns
+        -------
+        Type
+            The field type.
+        """
         return Type.from_handle(
             self._handle.as_struct_type().field_type(field_idx)
         )
 
     @property
     def aligned(self) -> bool:
+        r"""
+        Get whether the fields are aligned in memory.
+
+        Returns
+        -------
+        bool
+            `True` if the fields are aligned, `False` otherwise.
+        """
         return self._handle.as_struct_type().aligned()
 
     @property
     def offsets(self) -> tuple[uint32_t, ...]:
+        r"""
+        Get the memory offsets (in bytes) for each field in the struct.
+
+        Returns
+        -------
+        tuple[int, ...]
+            The offsets of the fields.
+        """
         return tuple(self._handle.as_struct_type().offsets())
 
     cpdef object to_numpy_dtype(self):
+        r"""
+        Get the equivalent numpy `dtype` for this type.
+
+        Returns
+        -------
+        np.dtype
+            The numpy dtype.
+
+        Raises
+        ------
+        ValueError
+            If the equivalent numpy `dtype` could not be determined.
+        """
         cdef uint32_t num_fields = self.num_fields
         # Need to construct these tuple using list comprehensions because
         # Cython (as of 3.0.8) does not yet support closures in cdef or cpdef
@@ -290,16 +533,59 @@ cdef class StructType(Type):
         )
 
 cpdef Type binary_type(uint32_t size):
+    r"""
+    Create a binary type.
+
+    Parameters
+    ----------
+    size : int
+        The size (in bytes) of the type.
+
+    Returns
+    -------
+    Type
+        The binary type.
+    """
     return Type.from_handle(_binary_type(size))
 
 
 cpdef FixedArrayType array_type(Type element_type, uint32_t N):
+    r"""
+    Create a fixed array type.
+
+    Parameters
+    ----------
+    element_type : Type
+        The type of the elements.
+    N : int
+        The number of elements.
+
+    Returns
+    -------
+    FixedArrayType
+        The fixed array type.
+    """
     return <FixedArrayType> Type.from_handle(
         _fixed_array_type(element_type._handle, N)
     )
 
 
 cpdef StructType struct_type(list field_types, bool align = True):
+    r"""
+    Create a structure type.
+
+    Parameters
+    ----------
+    field_types : list[Type]
+        The types of the fields.
+    align : bool (`True`)
+        Whether the types should be aligned in memory.
+
+    Returns
+    -------
+    StructType
+        The structure type.
+    """
     cdef std_vector[_Type] types = std_vector[_Type]()
     cdef Type field_type
 
@@ -314,8 +600,34 @@ cpdef StructType struct_type(list field_types, bool align = True):
 
 
 cpdef FixedArrayType point_type(int32_t ndim):
+    r"""
+    Create a point type.
+
+    Parameters
+    ----------
+    dim : int
+        The number of dimensions of the point.
+
+    Returns
+    -------
+    FixedArrayType
+        The point type.
+    """
     return FixedArrayType.from_handle(_point_type(ndim))
 
 
 cpdef StructType rect_type(int32_t ndim):
+    r"""
+    Create a rect type.
+
+    Parameters
+    ----------
+    ndim : int
+        The number of dimensions of rect.
+
+    Returns
+    -------
+    StructType
+        The rect type.
+    """
     return StructType.from_handle(_rect_type(ndim))
