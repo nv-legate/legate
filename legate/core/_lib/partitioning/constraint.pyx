6bc1446c7539cfcdfedfddcdd2ea20358cc443ff
Jacob Faibussowitsch <jacob.fai@gmail.com>
2024-08-27 10:18:15 -0400

    Document partitioning directory (#1140)
    * document partitioning directory

--- legate/core/_lib/partitioning/constraint.pyx
+++ legate/core/_lib/partitioning/constraint.pyx
@@ -27,9 +27,25 @@ cdef class Variable(Unconstructable):
         return result
 
     def __str__(self) -> str:
+        r"""
+        Return a human-readable representation of the variable.
+
+        Returns
+        -------
+        str
+            The human readable representation of the variable.
+        """
         return self._handle.to_string().decode()
 
     def __repr__(self) -> str:
+        r"""
+        Return a human-readable representation of the variable.
+
+        Returns
+        -------
+        str
+            The human readable representation of the variable.
+        """
         return str(self)
 
 
@@ -41,9 +57,25 @@ cdef class Constraint(Unconstructable):
         return result
 
     def __str__(self) -> str:
+        r"""
+        Return a human-readable representation of the constraint.
+
+        Returns
+        -------
+        str
+            The human readable representation of the constraint.
+        """
         return self._handle.to_string().decode()
 
     def __repr__(self) -> str:
+        r"""
+        Return a human-readable representation of the constraint.
+
+        Returns
+        -------
+        str
+            The human readable representation of the constraint.
+        """
         return str(self)
 
 
@@ -73,6 +105,34 @@ cdef class ConstraintProxy:
 
 
 cpdef object align(VariableOrStr lhs, VariableOrStr rhs):
+    r"""
+    Create an alignment constraint between two variables.
+
+    An alignment constraint between variables `x` and `y` indicates to the
+    runtime that the `PhysicalStore`s (leaf-task-local portions, typically
+    equal-size tiles) of the `LogicalStore`s corresponding to `x` and `y`
+    must have the same global indices (i.e. the stores must "align" with
+    one another).
+
+    This is commonly used for e.g. element-wise operations. For example,
+    consider an element-wise addition (`z = x + y`), where each array is 100
+    elements long. Each leaf task must receive the same local tile for all 3
+    arrays. For example, leaf task 0 receives indices `[0, 24)`, leaf task 1
+    receives `[25, 49)`, leaf task 2 receives `[50, 74)`, and leaf task 3
+    receives `[75, 99)`.
+
+    Parameters
+    ----------
+    lhs : Variable
+        The first variable to align.
+    rhs : Variable
+        The other variable to align.
+
+    Returns
+    -------
+    Constraint
+        The alignment constraint.
+    """
     if VariableOrStr is Variable:
         return Constraint.from_handle(_align(lhs._handle, rhs._handle))
     # I don't know why cython complains that this is unreachable. It is, just
@@ -83,6 +143,34 @@ cpdef object align(VariableOrStr lhs, VariableOrStr rhs):
 cpdef object broadcast(
     VariableOrStr variable, axes: Collection[int] = tuple()
 ):
+    r"""
+    Create a broadcast constraint on a variable.
+
+    A broadcast constraint informs the runtime that the variable should not be
+    split among the leaf tasks, instead, each leaf task should get a full copy
+    of the underlying store. In other words, the store should be "broadcast"
+    in its entirety to all leaf tasks in a task launch.
+
+    In effect, this constraint prevents all dimensions of the store from being
+    partitioned.
+
+    Parameters
+    ----------
+    variable : Variable
+        The variable to broadcast.
+    axes : Collection[int] (optional)
+        An optional set of axes which denotes a subset of the axes of
+        `variable` which to broadcast. If given, only the specified axes of
+        variable will be broadcast, all other axes will be partitioned
+        (subject to any other constraints). If not given (or if empty), all
+        axes will be broadcast.
+
+    Returns
+    -------
+    Constraint
+        The broadcast constraint.
+
+    """
     if not is_iterable(axes):
         raise ValueError("axes must be iterable")
 
@@ -107,6 +195,45 @@ cpdef object image(
     VariableOrStr var_range,
     ImageComputationHint hint = ImageComputationHint.NO_HINT,
 ):
+    r"""
+    Create an image constraint.
+
+    The elements of `var_function` are treated as pointers to elements in
+    `var_range`. Each sub-store `s` of `var_function` is aligned with a
+    sub-store `t` of `var_range`, such that every element in `s` will find the
+    element of `var_range` it's pointing to inside of `t`.
+
+    An approximate image of a function potentially contains extra points not in
+    the function's image. For example, if a function sub-store contains two 2-D
+    points `(0, 0)` and `(1, 1)`, the corresponding sub-store of the range
+    would only contain the elements at points `(0, 0)` and `(1, 1)` if it was
+    constructed from a precise image computation, whereas an approximate image
+    computation would yield a sub-store with elements at point `(0, 0)`,
+    `(0, 1)`, `(1, 0)`, and `(1, 1)` (two extra elements).
+
+    Currently, the precise image computation can be performed only by CPUs. As
+    a result, the function store is copied to the system memory if the store
+    was last updated by GPU tasks.  The approximate image computation has no
+    such issue and is fully GPU accelerated.
+
+    Parameters
+    ----------
+    var_function : Variable
+        Partition symbol for the function store.
+    var_range : Variable
+        Partition symbol of the store whose partition should be derived from
+        the image.
+    hint : ImageComputationHint (optional)
+        Hint to the runtime describing how the image computation can be
+        performed.  If no hint is given (which is the default), the runtime
+        falls back to the precise image computation. Otherwise, the runtime
+        computes a potentially approximate image of the function.
+
+    Returns
+    -------
+    Constraint
+        The image constraint.
+    """
     if VariableOrStr is Variable:
         return Constraint.from_handle(
             _image(var_function._handle, var_range._handle, hint)
@@ -119,6 +246,46 @@ cpdef object scale(
     VariableOrStr var_smaller,
     VariableOrStr var_bigger
 ):
+    r"""
+    Create a scaling constraint.
+
+    A scaling constraint is similar to an alignment constraint, except that the
+    sizes of the aligned tiles is first scaled by `factors`.
+
+    For example, this may be used in compacting a `5x56` array of `bool`s to a
+    `5x7` array of bytes, treated as a bitfield. In this case `var_smaller`
+    would be the byte array, `var_bigger` would be the array of `bool`s, and
+    `factors` would be `[1, 8]` (a `2x3` tile on the byte array corresponds to
+    a `2x24` tile on the bool array.
+
+    Formally: if two stores `A` and `B` are constrained by a scaling constraint
+    `scale(S, pA, pB)` where `pA` and `pB ` are partition symbols for `A` and
+    `B`, respectively, `A` and `B` will be partitioned such that each pair of
+    sub-stores `Ak` and `Bk` satisfy the following property:
+
+    ```{math}
+    \forall p \in \mathit{dom}(\mathtt{Ak}).
+    \forall \delta \in [-\mathtt{L}, \mathtt{H}].
+
+    p + \delta \in \mathit{dom}(\mathtt{Bk})
+    \lor p + \delta \not \in \mathit{dom}(\mathtt{B})
+    ```
+
+    Parameters
+    ----------
+    factors : tuple[int, ...]
+        The scaling factors.
+    var_smaller : Variable
+        Partition symbol for the smaller store (i.e., the one whose extents
+        are scaled).
+    var_bigger : Variable
+        Partition symbol for the bigger store.
+
+    Returns
+    -------
+    Constraint
+        The scaling constraint.
+    """
     if VariableOrStr is Variable:
         return Constraint.from_handle(
             _scale(
@@ -138,6 +305,48 @@ cpdef object bloat(
     tuple low_offsets,
     tuple high_offsets,
 ):
+    r"""
+    Create a bloat constraint.
+
+    This is typically used in stencil computations, to instruct the runtime
+    that the tiles on the "private + ghost" partition (`var_bloat`) must align
+    with the tiles on the "private" partition (`var_source`), but also include
+    a halo of additional elements off each end.
+
+    For example, if `var_source` and `var_bloat` correspond to 10-element
+    vectors, \p var_source is split into 2 tiles, `0-4` and `5-9`,
+    `low_offsets == 1` and `high_offsets == 2`, then `var_bloat` will be split
+    into 2 tiles, `0-6` and `4-9`.
+
+    Formally, if two stores `A` and `B` are constrained by a bloating
+    constraint `bloat(pA, pB, L, H)` where `pA` and `pB ` are partition symbols
+    for `A` and `B`, respectively, `A` and `B` will be partitioned such that
+    each pair of sub-stores `Ak` and `Bk` satisfy the following property:
+
+    ```{math}
+    \forall p \in \mathit{dom}(\mathtt{Ak}).
+    \forall \delta \in [-\mathtt{L}, \mathtt{H}].
+
+    p + \delta \in \mathit{dom}(\mathtt{Bk}) \lor
+    p + \delta \not \in \mathit{dom}(\mathtt{B})
+    ```
+
+    Parameters
+    ----------
+    var_source : Variable
+        Partition symbol for the source store.
+    var_bloat : Variable
+        Partition symbol for the target store.
+    low_offsets : tuple[int, ...]
+        Offsets to bloat towards the negative direction.
+    high_offsets : tuple[int, ...]
+        Offsets to bloat towards the positive direction.
+
+    Returns
+    -------
+    Constraint
+        The bloat constraint.
+    """
     if VariableOrStr is Variable:
         return Constraint.from_handle(
             _bloat(
