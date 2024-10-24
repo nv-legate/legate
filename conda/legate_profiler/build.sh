2d31ac6d715767763738ba0fe178361b614134ee
Marcin Zalewski <marcin.zalewski@gmail.com>
2024-10-24 13:58:16 -0700

    Fix profiler build (#1393)
    * Add a clang requirement
    
    * Run workflow
    
    * clang and rust
    
    * Update conda/legate_profiler/meta.yaml
    
    * Update conda/legate_profiler/meta.yaml
    
    * Update conda/legate_profiler/meta.yaml
    
    * more
    
    * Update gh-legate-profiler.yml
    
    Remove libssl-dev from aptitude install command
    
    * mambaforge and move deps
    
    * conda-verify and more libclang
    
    * fix
    
    * Try clang path
    
    * no libclang in host
    
    * undo
    
    * re-add a line
    
    ---------
    
    Co-authored-by: Evan Ramos <evanr@nvidia.com>

--- conda/legate_profiler/build.sh
+++ conda/legate_profiler/build.sh
@@ -12,5 +12,5 @@ echo "Legion checked-out with commit: ${GIT_COMMIT}"
 
 # Navigate to the legate-profiler directory and build it
 # shellcheck disable=SC2154
-cargo install --debug --path "${SRC_DIR}"/tools/legion_prof_rs --all-features --root "${PREFIX}"
+LIBCLANG_PATH=${BUILD_PREFIX}/lib cargo install --debug --path "${SRC_DIR}"/tools/legion_prof_rs --all-features --root "${PREFIX}"
 echo "Done"
