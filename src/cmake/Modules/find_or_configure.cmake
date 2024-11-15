49ea96ba2f3a460776761974b564c075c3ef7462
Jacob Faibussowitsch <jacob.fai@gmail.com>
2024-11-15 13:56:25 -0600

    Fix repeated pip install bricking on passing wrong `GIT_REPOSITORY` arguments to `CPMAddPackage()` (#1503)

--- src/cmake/Modules/find_or_configure.cmake
+++ src/cmake/Modules/find_or_configure.cmake
@@ -64,21 +64,16 @@ macro(legate_find_or_configure)
     # We do need to be careful about using rapids_find_package(), however, since that
     # still calls find_package(). Each of the packages should therefore do:
     #
-    # if(NOT CPM_DOWNLOAD_Foo AND NOT CPM_Foo_SOURCE) rapids_find_package(Foo) else()
-    # rapids_cpm_find(Foo) endif()
+    # cmake-format: off
+    # if(NOT CPM_DOWNLOAD_Foo)
+    #   rapids_find_package(Foo)
+    # else()
+    #   rapids_cpm_find(Foo)
+    # endif()
+    # cmake-format: on
     message(STATUS "${_LEGATE_FOC_PACKAGE}_DIR and ${_LEGATE_FOC_PACKAGE}_ROOT undefined, "
                    "forcing CPM to re-use downloaded ${_LEGATE_FOC_PACKAGE} from now on")
     set(CPM_DOWNLOAD_${_LEGATE_FOC_PACKAGE} ON CACHE BOOL "" FORCE)
-    if(CPM_PACKAGE_${_LEGATE_FOC_PACKAGE}_SOURCE_DIR)
-      # If the local package path was supplied by the user, this will be populated to the
-      # right place.
-      set(CPM_${_LEGATE_FOC_PACKAGE}_SOURCE
-          "${CPM_PACKAGE_${_LEGATE_FOC_PACKAGE}_SOURCE_DIR}" CACHE PATH "" FORCE)
-    else()
-      set(CPM_${_LEGATE_FOC_PACKAGE}_SOURCE
-          "${FETCHCONTENT_BASE_DIR}/${_LEGATE_FOC_PACKAGE_LOWER}-src" CACHE PATH "" FORCE)
-    endif()
-
   endif()
 
   unset(_LEGATE_FOC_PACKAGE)
