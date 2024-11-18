f68be33d5571912263fe3f801bfd3de7a8a05cdb
Jacob Faibussowitsch <jacob.fai@gmail.com>
2024-11-18 07:36:22 -0600

    Automatically add license files to Sphinx docs (#1502)
    * Automatically add license files to Sphinx docs

---
+++ src/cmake/thirdparty/get_cpm_licenses.cmake
@@ -0,0 +1,26 @@
+#=============================================================================
+# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: LicenseRef-NvidiaProprietary
+#
+# NVIDIA CORPORATION, its affiliates and licensors retain all intellectual
+# property and proprietary rights in and to this material, related
+# documentation and any modifications thereto. Any use, reproduction,
+# disclosure or distribution of this material and related documentation
+# without an express license agreement from NVIDIA CORPORATION or
+# its affiliates is strictly prohibited.
+#=============================================================================
+
+include_guard(GLOBAL)
+
+function(find_or_configure_cpm_licenses)
+  list(APPEND CMAKE_MESSAGE_CONTEXT "cpm_licenses")
+
+  include("${rapids-cmake-dir}/cpm/detail/package_details.cmake")
+  rapids_cpm_package_details(cpm_licenses version git_url git_tag git_shallow unused)
+
+  rapids_cpm_find(cpm_licenses "${version}"
+                  CPM_ARGS
+                  GIT_REPOSITORY "${git_url}"
+                  GIT_SHALLOW "${git_shallow}" SYSTEM TRUE
+                  GIT_TAG "${git_tag}")
+endfunction()
