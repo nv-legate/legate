a34f0cfcaef917a352cb486a06452d0d33fc6da5
Marcin Zalewski <marcin.zalewski@gmail.com>
2025-01-16 19:39:30 -0800

    Do not set errexit in activation scrpts (#1694)
    * do not modify the shell in activation scripts
    
    * Increase build numbers

--- conda/mpi_wrapper/activate.sh
+++ conda/mpi_wrapper/activate.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 echo -e "\n\n--------------------- CONDA/MPI_WRAPPER/ACTIVATE.SH -----------------------\n"
 
-set -eo pipefail
 # shellcheck disable=SC2154
 wrapper_file=$(find "${CONDA_PREFIX}/mpi-wrapper" -regex ".*/liblegate_mpi_wrapper\.\(so\|dylib\)" -print -quit)
 export LEGATE_MPI_WRAPPER="${wrapper_file}"
