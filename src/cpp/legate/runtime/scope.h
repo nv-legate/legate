6a7237fd070268b6a71761bf30d7236ebb36f2e1
Jacob Faibussowitsch <jacob.fai@gmail.com>
2025-03-03 11:43:15 -0500

    Add pragma once to scope.h (#1902)

--- src/cpp/legate/runtime/scope.h
+++ src/cpp/legate/runtime/scope.h
@@ -10,6 +10,8 @@
  * its affiliates is strictly prohibited.
  */
 
+#pragma once
+
 #include <legate/mapping/machine.h>
 #include <legate/runtime/exception_mode.h>
 #include <legate/utilities/detail/doxygen.h>
