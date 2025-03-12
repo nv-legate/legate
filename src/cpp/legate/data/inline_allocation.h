84135f443dd8f4a974269eda11b7a5972636bce8
Jacob Faibussowitsch <jacob.fai@gmail.com>
2025-03-12 13:14:10 -0400

    Better InlineAllocation documentation (#1973)

--- src/cpp/legate/data/inline_allocation.h
+++ src/cpp/legate/data/inline_allocation.h
@@ -19,12 +19,29 @@ namespace legate {
  */
 
 /**
- * @brief An object representing the raw memory and strides held by a `PhysicalStore`
+ * @brief An object representing the raw memory and strides held by a `PhysicalStore`.
  */
 class InlineAllocation {
  public:
-  void* ptr{};                        /**< pointer to the start of the allocation */
-  std::vector<std::size_t> strides{}; /**< vector of offsets into the buffer */
+  /**
+   * @brief pointer to the start of the allocation.
+   */
+  void* ptr{};
+
+  /**
+   * @brief vector of byte-offsets into `ptr`.
+   *
+   * The offsets are given in bytes, and represent the number of bytes needed to jump to the
+   * next array element in the corresponding dimension. For example:
+   *
+   * #. For an array whose entries are 4 bytes long, and whose shape is `(1,)`, `strides` would
+   *    be `[4]` (entry `i` given by `4*i`).
+   * #. For an array whose entries are 8 bytes long, and whose shape is `(1, 2,)`, `strides` would
+   *    be `[16, 8]` (entry `(i, j)` given by `16*i + 8*j`).
+   * #. For an array whose entries are 8 bytes long, and whose shape is `(1, 2, 3)`,
+   *    strides` would be `[48, 24, 8]` (entry `(i, j, k)` given by `48*i + 24*j + 8*k`).
+   */
+  std::vector<std::size_t> strides{};
 };
 
 /** @} */
