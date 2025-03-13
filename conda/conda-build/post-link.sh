eac9d0060b8f6a1e17609c710b2f2e07f5334781
Manolis Papadakis <mpapadakis@nvidia.com>
2025-03-12 23:40:11 -0700

    Remove EULA (#1979)

--- conda/conda-build/post-link.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# shellcheck disable=SC2154
-echo "By downloading and using the ${PKG_NAME} conda package, you accept the terms and conditions of the NVIDIA Legate Evaluation License Agreement - https://docs.nvidia.com/legate/eula.pdf" > "${PREFIX}/.messages.txt"
