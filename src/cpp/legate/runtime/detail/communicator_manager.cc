nv-legate/legate

commit 6a9ab0cf7411f69dfb817bbc3ceff56a4bdc3e59

[BUG] ensure correct teardown order for communicator factories (#1975)

src/cpp/legate/runtime/detail/communicator_manager.cc
@@ -100,6 +100,9 @@ void CommunicatorManager::destroy()
 {
   for (auto it = factories_.rbegin(); it != factories_.rend(); ++it) {
     it->second->destroy();
+    // ensure reverse task order due to dependencies (CAL->CPU)
+    // without fence CPU teardown might supercede CAL
+    Runtime::get_runtime()->issue_execution_fence();
   }
   factories_.clear();
 }

tests/cpp/integration/cal_communicator.cu
@@ -1,0 +1,102 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2023-2025 NVIDIA CORPORATION & AFFILIATES. All rights
+ * reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <legate.h>
+
+#include <legate/comm/coll.h>
+
+#include <gtest/gtest.h>
+
+#include <cal.h>
+#include <utilities/utilities.h>
+
+namespace cal_communicator {
+
+namespace {
+
+constexpr std::size_t SIZE = 100;
+
+class CALCommunicatorTester : public legate::LegateTask<CALCommunicatorTester> {
+ public:
+  static constexpr auto TASK_ID = legate::LocalTaskID{};
+
+  static constexpr auto GPU_VARIANT_OPTIONS =
+    legate::VariantOptions{}.with_concurrent(true).with_has_allocations(true);
+
+  static void gpu_variant(legate::TaskContext context)
+  {
+    if (context.is_single_task()) {
+      GTEST_SKIP();
+    }
+
+    EXPECT_TRUE(context.communicators().size() == 2);
+
+    auto cal_comm = context.communicators().at(1).get<cal_comm_t>();
+
+    const auto num_tasks = context.get_launch_domain().get_volume();
+    int num_ranks;
+    const calError_t status = cal_comm_get_size(cal_comm, &num_ranks);
+    EXPECT_EQ(status, CAL_OK);
+    EXPECT_EQ(num_ranks, num_tasks);
+  }
+};
+
+class Config {
+ public:
+  static constexpr std::string_view LIBRARY_NAME = "test_cal_communicator";
+  static void registration_callback(legate::Library library)
+  {
+    CALCommunicatorTester::register_variants(library);
+  }
+};
+
+class CALCommunicator : public RegisterOnceFixture<Config> {};
+
+class CalCommunicatorManualTaskTest : public CALCommunicator,
+                                      public ::testing::WithParamInterface<std::int32_t> {};
+
+INSTANTIATE_TEST_SUITE_P(CALCommunicator,
+                         CalCommunicatorManualTaskTest,
+                         ::testing::Values(1, 2, 3));
+
+TEST_P(CalCommunicatorManualTaskTest, CAL_communicator)
+{
+  auto runtime = legate::Runtime::get_runtime();
+  auto machine = runtime->get_machine();
+
+  if (machine.count(legate::mapping::TaskTarget::GPU) == 0) {
+    GTEST_SKIP() << "No GPUs available. CAL communicator tests require GPUs.";
+  }
+
+  legate::Scope scope{machine.only(legate::mapping::TaskTarget::GPU)};
+
+  const auto num_procs = runtime->get_machine().count();
+  if (num_procs <= 1) {
+    GTEST_SKIP() << "This test requires more than one processor.";
+  }
+
+  auto ndim = GetParam();
+
+  auto context = runtime->find_library(Config::LIBRARY_NAME);
+  auto store =
+    runtime->create_store(legate::Shape{legate::full<std::uint64_t>(ndim, SIZE)}, legate::int32());
+  auto launch_shape = legate::full<std::uint64_t>(ndim, 1);
+  auto tile_shape   = legate::full<std::uint64_t>(ndim, 1);
+  launch_shape[0]   = num_procs;
+  tile_shape[0]     = (SIZE + num_procs - 1) / num_procs;
+
+  auto part = store.partition_by_tiling(tile_shape.data());
+
+  auto task = runtime->create_task(context, CALCommunicatorTester::TASK_ID, launch_shape);
+  task.add_output(part);
+  task.add_communicator("nccl");
+  task.add_communicator("cal");
+  runtime->submit(std::move(task));
+}
+
+}  // namespace
+
+}  // namespace cal_communicator

continuous_integration/scripts/build
@@ -87,6 +87,7 @@ build_legate_release() {
   conda_build_args+=(--quiet);
   conda_build_args+=(--override-channels);
 
+  conda_build_args+=(-c legate/label/ucc140);
   conda_build_args+=(-c conda-forge);
 
   conda_build_args+=(--croot /tmp/conda-croot/legate);

continuous_integration/scripts/test
@@ -107,6 +107,22 @@ run_test_or_analysis() {
       fi
     }
 
+    case "$2" in
+      "cpu")
+        export GPUS_COUNT=0
+        ;;
+      "gpu")
+        export GPUS_COUNT=1
+        ;;
+      "2gpu")
+        export GPUS_COUNT=2
+        ;;
+      *)
+        echo "Invalid argument: $2"
+        return 2
+        ;;
+    esac
+
     case "$1" in
         "python")
             echo "Executing python tests..."
@@ -139,16 +155,16 @@ run_test_or_analysis() {
 
             set_ld_preload
 
-            case "$2" in
-              "cpu")
+            case "${GPUS_COUNT}" in
+              "0")
                 echo "Using CPUs ..."
                 LEGATE_AUTO_CONFIG=0 pytest --color=yes tests/python -s
                 ;;
-              "gpu")
+              "1")
                 echo "Using 1 GPU ..."
                 LEGATE_AUTO_CONFIG=0 LEGATE_CONFIG="--fbmem 4000 --gpus 1" pytest --color=yes tests/python -s
                 ;;
-              "2gpu")
+              "2")
                 echo "Using 2 GPUs ..."
                 LEGATE_AUTO_CONFIG=0 LEGATE_CONFIG="--fbmem 4000 --gpus 2" pytest --color=yes tests/python -s
                 ;;
@@ -183,7 +199,7 @@ run_test_or_analysis() {
               local worker_count="${4:-2}"
               LEGATE_TEST=1 \
                 LEGATE_AUTO_CONFIG=0 \
-                LEGATE_CONFIG="--${proc_type} ${proc_count} --sysmem 1000 --fbmem 1000" \
+                LEGATE_CONFIG="--${proc_type} ${proc_count} --sysmem 4000 --fbmem 1000" \
                 "${gtest_file}" --gtest_color=yes
             }
 
@@ -226,23 +242,29 @@ run_test_or_analysis() {
 
             function run_all_runtime_tests()
             {
-              run_cpp_tests cpus 1
-              # Also do some multi-CPU testing
-              uname_value=$(uname)
-              if [[ "${uname_value}" == "Darwin" ]]; then
-                run_cpp_tests cpus 2
-              else
-                run_cpp_tests cpus 4
+              if [[ "${GPUS_COUNT}" -ne 2 ]]; then
+                run_cpp_tests cpus 1
+                # Also do some multi-CPU testing
+                uname_value=$(uname)
+                if [[ "${uname_value}" == "Darwin" ]]; then
+                  run_cpp_tests cpus 2
+                else
+                  run_cpp_tests cpus 4
+                fi
               fi
 
-              if [[ -f ${CONDA_PREFIX}/bin/tests_with_gpus ]]; then
-                run_gpu_tests gpus 1
+              if [[ "${GPUS_COUNT}" -gt 0 ]]; then
+                if [[ -f ${CONDA_PREFIX}/bin/tests_with_gpus ]]; then
+                  run_gpu_tests gpus "${GPUS_COUNT}"
+                fi
               fi
             }
 
             set_ld_preload
 
-            run_sequential_tests
+            if [[ "${GPUS_COUNT}" -ne 2 ]]; then
+              run_sequential_tests
+            fi
 
             export LEGATE_WINDOW_SIZE=1
             run_all_runtime_tests

continuous_integration/scripts/tools/legate-conda-utils
@@ -90,6 +90,7 @@ install_legate_with_war() {
   legate-mamba-retry install \
                      -y \
                      -n "${CONDA_ENV}" \
+                     -c legate/label/ucc140 \
                      -c conda-forge \
                      -c "${ARTIFACTS_DIR}/conda-build/legate" \
                      legate

src/python/legate/driver/launcher.py
@@ -175,6 +175,12 @@ class Launcher:
         # as the former plays better with Realm.
         env["NCCL_LAUNCH_MODE"] = "PARALLEL"
 
+        # Make sure we do not use the CUDA_TL in UCC
+        # CUDA_TL currently only works in a single-thread setting.
+        # CUDA_TL could work with Legate multi-rank where every rank has a
+        # single thread, but we just blanket disable it.
+        env["UCC_TLS"] = "^cuda"
+
         # Make sure GASNet initializes MPI with the right level of
         # threading support
         env["GASNET_MPI_THREAD"] = "MPI_THREAD_MULTIPLE"

tests/cpp/CMakeLists.txt
@@ -73,6 +73,10 @@ function(legate_configure_test)
     target_link_libraries(${_LEGATE_TEST_NAME} PRIVATE NCCL::NCCL)
   endif()
 
+  if(legate_USE_CAL)
+    target_link_libraries(${_LEGATE_TEST_NAME} PRIVATE CAL::CAL)
+  endif()
+
   rapids_test_add(NAME ${_LEGATE_TEST_NAME}
                   COMMAND ${_LEGATE_TEST_NAME}
                   GPUS ${_LEGATE_TEST_GPUS}
@@ -298,11 +302,16 @@ legate_configure_test(NAME tests_non_reentrant_with_runtime
 legate_configure_test(NAME tests_non_reentrant_wo_runtime
                       SOURCES main_wo_runtime_init.cc ${non_reentrant_wo_runtime_SRC})
 
+if(legate_USE_CAL)
+  set(with_cal_SRC integration/cal_communicator.cu)
+endif()
+
 if(Legion_USE_CUDA)
   set(gpu_SRC integration/nccl.cu)
 
   legate_configure_test(NAME tests_with_gpus
                         SOURCES main_with_runtime_init.cc ${with_runtime_SRC} ${gpu_SRC}
+                                ${with_cal_SRC}
                         GPUS 1
                         PERCENT 30
                         ADDITIONAL_TARGETS HighFive::HighFive)

tests/python/unit/legate/driver/test_launcher.py
@@ -161,6 +161,13 @@ class TestLauncherEnv:
 
         assert env["NCCL_LAUNCH_MODE"] == "PARALLEL"
 
+    def test_uss_tls(self, genconfig: GenConfig, launch: LauncherType) -> None:
+        config = genconfig(["--launcher", launch])
+
+        env = m.Launcher.create(config, SYSTEM).env
+
+        assert env["UCC_TLS"] == "^cuda"
+
     def test_gasnet_mpi_thread(
         self, genconfig: GenConfig, launch: LauncherType
     ) -> None:
@@ -352,6 +359,8 @@ class TestMPILauncher:
         "PYTHONPATH",
         "-x",
         "NCCL_LAUNCH_MODE",
+        "-x",
+        "UCC_TLS",
         "-x",
         "GASNET_MPI_THREAD",
         "-x",

commit f4afbba8b674bc84dc497d30037ac88da5edcc67

Fix errors introduced by #1975 (#1999)

src/cpp/legate/runtime/detail/communicator_manager.cc
@@ -101,7 +101,7 @@ void CommunicatorManager::destroy()
   for (auto it = factories_.rbegin(); it != factories_.rend(); ++it) {
     it->second->destroy();
     // ensure reverse task order due to dependencies (CAL->CPU)
-    // without fence CPU teardown might supercede CAL
+    // without fence CPU teardown might supersede CAL
     Runtime::get_runtime()->issue_execution_fence();
   }
   factories_.clear();

tests/cpp/integration/cal_communicator.cu
@@ -21,7 +21,8 @@ constexpr std::size_t SIZE = 100;
 
 class CALCommunicatorTester : public legate::LegateTask<CALCommunicatorTester> {
  public:
-  static constexpr auto TASK_ID = legate::LocalTaskID{};
+  static inline const auto TASK_CONFIG =  // NOLINT(cert-err58-cpp)
+    legate::TaskConfig{legate::LocalTaskID{0}};
 
   static constexpr auto GPU_VARIANT_OPTIONS =
     legate::VariantOptions{}.with_concurrent(true).with_has_allocations(true);
@@ -49,7 +50,7 @@ class Config {
   static constexpr std::string_view LIBRARY_NAME = "test_cal_communicator";
   static void registration_callback(legate::Library library)
   {
-    CALCommunicatorTester::register_variants(library);
+    CALCommunicatorTester::register_variants(library, CALCommunicatorTester::TASK_CONFIG.task_id());
   }
 };
 
@@ -90,7 +91,8 @@ TEST_P(CalCommunicatorManualTaskTest, CAL_communicator)
 
   auto part = store.partition_by_tiling(tile_shape.data());
 
-  auto task = runtime->create_task(context, CALCommunicatorTester::TASK_ID, launch_shape);
+  auto task =
+    runtime->create_task(context, CALCommunicatorTester::TASK_CONFIG.task_id(), launch_shape);
   task.add_output(part);
   task.add_communicator("nccl");
   task.add_communicator("cal");