2ac541f8feefb992011e626a22a5d3c37c8b9938
Jacob Faibussowitsch <jacob.fai@gmail.com>
2025-06-04 16:05:13 -0400

    Escape quotes when dumping to JSON (#2336)

--- src/cmake/Modules/export_aedifix_post_config.cmake
+++ src/cmake/Modules/export_aedifix_post_config.cmake
@@ -5,6 +5,17 @@
 
 include_guard(GLOBAL)
 
+# See https://discourse.cmake.org/t/escaping-strings-for-output-to-json-file/5297
+function(_legate_json_esc ret_var val_str)
+  string(JSON tmp_json SET "{}" "${val_str}" "0")
+  string(REGEX REPLACE "^\\{[ \t\r\n]*" "" tmp_json "${tmp_json}")
+  string(REGEX REPLACE "[ \t\r\n]*:[ \t\r\n]*0[ \t\r\n]*\\}$" "" tmp_json "${tmp_json}")
+  if(NOT "${tmp_json}" MATCHES "^\"[^\n]*\"")
+    message(FATAL_ERROR "Internal error: unexpected output: '${tmp_json}'")
+  endif()
+  set(${ret_var} "${tmp_json}" PARENT_SCOPE)
+endfunction()
+
 function(legate_export_aedifix_post_config)
   list(APPEND CMAKE_MESSAGE_CONTEXT "export_aedifix_post_config")
 
@@ -15,7 +26,8 @@ function(legate_export_aedifix_post_config)
 
   set(data "{}")
   foreach(var IN LISTS AEDIFIX_EXPORT_VARIABLES)
-    string(JSON data SET "${data}" "${var}" "\"${${var}}\"")
+    _legate_json_esc(value "${${var}}")
+    string(JSON data SET "${data}" "${var}" "${value}")
   endforeach()
   message(VERBOSE "Wrote aedifix export config to ${AEDIFIX_EXPORT_CONFIG_PATH}")
   file(WRITE "${AEDIFIX_EXPORT_CONFIG_PATH}" "${data}")
