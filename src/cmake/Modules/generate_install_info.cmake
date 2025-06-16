dd611fe9a9ec5817583f6f4ba8eaef7ec7539736
Jacob Faibussowitsch <jacob.fai@gmail.com>
2025-06-16 15:17:05 -0400

    Minor CMake maintenance (#2370)
    * Minor CMake maintenance

---
+++ src/cmake/Modules/generate_install_info.cmake
@@ -0,0 +1,43 @@
+#=============================================================================
+# SPDX-FileCopyrightText: Copyright (c) 2024-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#=============================================================================
+
+include_guard(GLOBAL)
+
+function(legate_generate_install_info_py)
+  list(APPEND CMAKE_MESSAGE_CONTEXT "generate_install_info_py")
+  # Legion sets this to "OFF" if not enabled, normalize it to an empty list instead
+  if(NOT Legion_NETWORKS)
+    set(Legion_NETWORKS "")
+  endif()
+
+  # Set by the pip wheels if they are being built, set to OFF if not
+  if(NOT LEGATE_BUILD_PIP_WHEELS)
+    set(LEGATE_BUILD_PIP_WHEELS OFF)
+  endif()
+
+  add_custom_target(generate_install_info_py ALL
+                    COMMAND ${CMAKE_COMMAND} -DLEGATE_CMAKE_DIR="${LEGATE_CMAKE_DIR}"
+                            -DLEGATE_ARCH="${LEGATE_ARCH}" -DLEGATE_DIR="${LEGATE_DIR}"
+                            -DLEGATE_BUILD_PIP_WHEELS="${LEGATE_BUILD_PIP_WHEELS}"
+                            -DLegion_VERSION="${Legion_VERSION}"
+                            -DLegion_GIT_BRANCH="${Legion_GIT_BRANCH}"
+                            -DLegion_GIT_REPO="${Legion_GIT_REPO}"
+                            -DLegion_NETWORKS="${Legion_NETWORKS}"
+                            -DGASNet_CONDUIT="${GASNet_CONDUIT}"
+                            -DLegion_USE_CUDA="${Legion_USE_CUDA}"
+                            -DLegion_USE_OpenMP="${Legion_USE_OpenMP}"
+                            -DLegion_MAX_DIM="${Legion_MAX_DIM}"
+                            -DLegion_MAX_FIELDS="${Legion_MAX_FIELDS}"
+                            -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}"
+                            -DCMAKE_C_COMPILER="${CMAKE_C_COMPILER}"
+                            -DCMAKE_CXX_COMPILER="${CMAKE_CXX_COMPILER}"
+                            -Dlegate_LIB_NAME="$<TARGET_FILE_PREFIX:legate::legate>$<TARGET_FILE_BASE_NAME:legate::legate>"
+                            -Dlegate_FULL_LIB_NAME="$<TARGET_FILE_NAME:legate::legate>" -P
+                            "${LEGATE_CMAKE_DIR}/generate_install_info_py.cmake"
+                    DEPENDS "${LEGATE_CMAKE_DIR}/templates/install_info.py.in"
+                    BYPRODUCTS "${LEGATE_DIR}/src/python/legate/install_info.py"
+                    COMMENT "Generate install_info.py")
+
+endfunction()
--- src/cmake/thirdparty/get_cccl.cmake
+++ src/cmake/thirdparty/get_cccl.cmake
@@ -11,7 +11,42 @@ function(find_or_configure_cccl)
 
   include(${rapids-cmake-dir}/cpm/cccl.cmake)
 
+  if(NOT legate_checked_for_Legion_USE_OpenMP)
+    message(FATAL_ERROR "Legion_USE_OpenMP must have been defined already by this point, to properly set the thrust host system settings"
+    )
+  endif()
+
+  # Change THRUST_DEVICE_SYSTEM for `.cpp` files If we include Thrust in "CUDA mode" in
+  # .cc files, that ends up pulling the definition of __half from the CUDA toolkit, and
+  # Legion defines a custom __half when compiling outside of nvcc (because CUDA's __half
+  # doesn't define any __host__ functions), which causes a conflict.
+  if(Legion_USE_OpenMP)
+    set(CCCL_THRUST_DEVICE_SYSTEM "OMP")
+  else()
+    set(CCCL_THRUST_DEVICE_SYSTEM "CPP")
+  endif()
+
   rapids_cpm_cccl(BUILD_EXPORT_SET legate-exports INSTALL_EXPORT_SET legate-exports SYSTEM
                                                                                     TRUE)
+
+  # Workaround for https://github.com/NVIDIA/cccl/issues/5002
+  if(Legion_USE_OpenMP)
+    get_target_property(opts OpenMP::OpenMP_CXX INTERFACE_COMPILE_OPTIONS)
+    string(REPLACE [[-Xcompiler=SHELL:]] [[SHELL:-Xcompiler=]] opts "${opts}")
+    # cmake-lint barfs for some reason here, even though cmake-format is the one that
+    # formatted the source to begin with...
+    #
+    # cmake-format: off
+    # path/to/get_cccl.cmake:37,65: [C0307] Bad indentation:
+    #                                                        )
+    #    ^----BodyNode: 1:0->FlowControlNode: 9:0->BodyNode: 9:32->IfBlockNode:
+    #    33:2->BodyNode: 33:23->StatementNode: 36:4->TreeNode: 37:65
+    # cmake-format: on
+    #
+    # cmake-lint: disable=C0307
+    set_target_properties(OpenMP::OpenMP_CXX PROPERTIES INTERFACE_COMPILE_OPTIONS
+                                                        "${opts}")
+  endif()
+
   legate_export_variables(CCCL)
 endfunction()
