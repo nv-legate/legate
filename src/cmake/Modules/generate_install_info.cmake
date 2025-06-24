bb20ba43fb92cbb77368d8391c72ada23b01455f
Jacob Faibussowitsch <jacob.fai@gmail.com>
2025-06-24 09:47:24 -0400

    Create install_info.py without a dummy target (#2382)

--- src/cmake/Modules/generate_install_info.cmake
+++ src/cmake/Modules/generate_install_info.cmake
@@ -22,30 +22,24 @@ function(legate_generate_install_info_py)
   # space-delimited string, which is what is desired. It is unclear if this CMake behavior
   # is documented/intendeed, but it it unlikely to change. Other approaches, e.g. VERBATIM
   # for the custom target, fail for other reasons.
-  string(REPLACE " " ";" legate_configure_options_list "${LEGATE_CONFIGURE_OPTIONS}")
+  string(REPLACE " " ";" LEGATE_CONFIGURE_OPTIONS "${LEGATE_CONFIGURE_OPTIONS}")
 
-  add_custom_target(generate_install_info_py ALL
-                    COMMAND ${CMAKE_COMMAND} -DLEGATE_CMAKE_DIR="${LEGATE_CMAKE_DIR}"
-                            -DLEGATE_ARCH="${LEGATE_ARCH}" -DLEGATE_DIR="${LEGATE_DIR}"
-                            -DLEGATE_BUILD_PIP_WHEELS="${LEGATE_BUILD_PIP_WHEELS}"
-                            -DLegion_VERSION="${Legion_VERSION}"
-                            -DLegion_GIT_BRANCH="${Legion_GIT_BRANCH}"
-                            -DLegion_GIT_REPO="${Legion_GIT_REPO}"
-                            -DLegion_NETWORKS="${Legion_NETWORKS}"
-                            -DGASNet_CONDUIT="${GASNet_CONDUIT}"
-                            -DLegion_USE_CUDA="${Legion_USE_CUDA}"
-                            -DLegion_USE_OpenMP="${Legion_USE_OpenMP}"
-                            -DLegion_MAX_DIM="${Legion_MAX_DIM}"
-                            -DLegion_MAX_FIELDS="${Legion_MAX_FIELDS}"
-                            -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}"
-                            -DCMAKE_C_COMPILER="${CMAKE_C_COMPILER}"
-                            -DCMAKE_CXX_COMPILER="${CMAKE_CXX_COMPILER}"
-                            -DLEGATE_CONFIGURE_OPTIONS="${legate_configure_options_list}"
-                            -Dlegate_LIB_NAME="$<TARGET_FILE_PREFIX:legate::legate>$<TARGET_FILE_BASE_NAME:legate::legate>"
-                            -Dlegate_FULL_LIB_NAME="$<TARGET_FILE_NAME:legate::legate>" -P
-                            "${LEGATE_CMAKE_DIR}/generate_install_info_py.cmake"
-                    DEPENDS "${LEGATE_CMAKE_DIR}/templates/install_info.py.in"
-                    BYPRODUCTS "${LEGATE_DIR}/src/python/legate/install_info.py"
-                    COMMENT "Generate install_info.py")
+  set(legate_LIB_NAME
+      "$<TARGET_FILE_PREFIX:legate::legate>$<TARGET_FILE_BASE_NAME:legate::legate>")
+  set(legate_FULL_LIB_NAME "$<TARGET_FILE_NAME:legate::legate>")
 
+  cmake_path(SET install_info_tmp NORMALIZE
+             "${CMAKE_CURRENT_BINARY_DIR}/install_info_tmp/install_info.py")
+  # We need this 2-step because we make use of generator expressions, which
+  # configure_file() does not support.
+  #
+  # The first configure_file() will emit the temporary file in our bin dir, replacing the
+  # cmake values verbatim. It will contain generator expressions...
+  configure_file("${LEGATE_CMAKE_DIR}/templates/install_info.py.in" "${install_info_tmp}"
+                 @ONLY)
+  # ...which this file(GENERATE) call will evaluate out. We could do this with a target,
+  # that calls a cmake script that calls configure_file(), but it's much cleaner to do
+  # this from the original generation stage.
+  file(GENERATE OUTPUT "${LEGATE_DIR}/src/python/legate/install_info.py"
+       INPUT "${install_info_tmp}")
 endfunction()
--- src/cmake/generate_install_info_py.cmake
+++
@@ -1,8 +0,0 @@
-#=============================================================================
-# SPDX-FileCopyrightText: Copyright (c) 2021-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-#=============================================================================
-
-set(libpath "")
-configure_file("${LEGATE_CMAKE_DIR}/templates/install_info.py.in"
-               "${LEGATE_DIR}/src/python/legate/install_info.py" @ONLY)
