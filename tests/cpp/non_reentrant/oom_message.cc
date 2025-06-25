400a443206db57b3967fb988ea3e0bdec0aa4e7f
Irina Demeshko <idemeshko@nvidia.com>
2025-06-25 06:06:59 -0700

    Revert "Pre-allocate task-temporary allocations (#2246)" (#2399)
    This reverts commit 383b0044d24b63342ac3170d990e988a35c1f36f.

--- tests/cpp/non_reentrant/oom_message.cc
+++ tests/cpp/non_reentrant/oom_message.cc
@@ -20,7 +20,6 @@ namespace {
 constexpr auto SMALL_SIZE    = 100;
 constexpr auto HUGE_EXPONENT = 50;
 constexpr auto HUGE_SIZE     = static_cast<std::uint64_t>(1) << HUGE_EXPONENT;  // 1 PiB
-constexpr auto LIBRARY_NAME  = "test_oom_message";
 
 class DummyTask : public legate::LegateTask<DummyTask> {
  public:
@@ -30,56 +29,21 @@ class DummyTask : public legate::LegateTask<DummyTask> {
   static void cpu_variant(legate::TaskContext) {}
 };
 
-class LargeBoundedPoolTask : public legate::LegateTask<LargeBoundedPoolTask> {
+class Config {
  public:
-  static inline const auto TASK_CONFIG =  // NOLINT(cert-err58-cpp)
-    legate::TaskConfig{legate::LocalTaskID{1}}.with_variant_options(
-      legate::VariantOptions{}.with_has_allocations(true));
-
-  static void cpu_variant(legate::TaskContext) {}
-};
-
-class LibraryMapper : public legate::mapping::Mapper {
- public:
-  std::vector<legate::mapping::StoreMapping> store_mappings(
-    const legate::mapping::Task& /*task*/,
-    const std::vector<legate::mapping::StoreTarget>& /*options*/) override
+  static constexpr std::string_view LIBRARY_NAME = "test_oom_message";
+  static void registration_callback(legate::Library library)
   {
-    return {};
-  }
-  legate::Scalar tunable_value(legate::TunableID /*tunable_id*/) override
-  {
-    return legate::Scalar{};
-  }
-  std::optional<std::size_t> allocation_pool_size(
-    const legate::mapping::Task& task, legate::mapping::StoreTarget /*memory_kind*/) override
-  {
-    // This is the only task in this Library that was declared to have temporary allocations.
-    LEGATE_CHECK(task.task_id() == LargeBoundedPoolTask::TASK_CONFIG.task_id());
-    return HUGE_SIZE;
+    DummyTask::register_variants(library);
   }
 };
 
-class OomMessageDeathTest : public DefaultFixture {
- public:
-  void SetUp() override
-  {
-    DefaultFixture::SetUp();
-    auto runtime = legate::Runtime::get_runtime();
-    auto created = false;
-    auto library = runtime->find_or_create_library(
-      LIBRARY_NAME, legate::ResourceConfig{}, std::make_unique<LibraryMapper>(), {}, &created);
-    if (created) {
-      DummyTask::register_variants(library);
-      LargeBoundedPoolTask::register_variants(library);
-    }
-  }
-};
+class OomMessageDeathTest : public RegisterOnceFixture<Config> {};
 
 void test_oom_message()
 {
   auto runtime = legate::Runtime::get_runtime();
-  auto library = runtime->find_library(LIBRARY_NAME);
+  auto library = runtime->find_library(Config::LIBRARY_NAME);
 
   // Declare stores (none get initialized until used)
   std::vector<legate::LogicalStore> stores;
@@ -160,27 +124,6 @@ TEST_F(OomMessageDeathTest, Simple)
       ::testing::ContainsRegex("Instance .* of size 99 covering elements <1>\\.\\.<99>.*"),
       ::testing::ContainsRegex(
         "created for an operation launched at launch_using_small_store_part")));
-}
-
-// This test hangs pretty frequently in CI, so disable it for now.
-TEST_F(OomMessageDeathTest, DISABLED_BoundedPool)
-{
-  auto runtime = legate::Runtime::get_runtime();
-  auto library = runtime->find_library(LIBRARY_NAME);
-
-  if (legate::detail::experimental::LEGATE_INLINE_TASK_LAUNCH.get(/* default_value =*/false)) {
-    GTEST_SKIP() << "Test requires Legion task launch";
-  }
-
-  auto task = runtime->create_task(library, LargeBoundedPoolTask::TASK_CONFIG.task_id());
-
-  EXPECT_DEATH(
-    {
-      runtime->submit(std::move(task));
-      runtime->issue_execution_fence(/*block=*/true);
-    },
-    ::testing::ContainsRegex(
-      "Failed to allocate .* for the temporary allocations of Task .*LargeBoundedPoolTask"));
 }
 
 }  // namespace oom_message
