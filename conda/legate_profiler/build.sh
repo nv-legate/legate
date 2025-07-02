98c94b83be7a4033c1ec710e491870c0dd61e946
Parag Kulkarni <pkulkarni@nvidia.com>
2025-07-02 20:28:30 +0530

    Include nsys support for Legate profiler. (#2388)
    * Include nsys support for Legate profiler.
    
    * s/GPU_ENABLED/BUILD_WITH_CUDA as per Marcin's suggestion.
    
    * Clean up lint errors (1)
    
    * Cleanup lint issues (2)
    
    * shellcheck disable=SC2154
    
    * Trailing space

--- conda/legate_profiler/build.sh
+++ conda/legate_profiler/build.sh
@@ -1,16 +1,38 @@
 #!/usr/bin/env bash
 
-echo -e "\n\n--------------------- CONDA/LEGATE-PROFILER/BUILD.SH -----------------------\n"
-
+echo -e "\n\n--------------------- CONDA/LEGATE-PROFILER/BUILD.SH (nsys) -----------------------\n"
 set -xeo pipefail
 
+# Set CUDA environment explicitly (important for cmake detection)
+NVCC_PATH=$(command -v nvcc) || exit 1
+export CUDACXX="${NVCC_PATH}"
+
+# Needed for cargo
+export CMAKE_CUDA_COMPILER=${CUDACXX}
+
 # Build the legate profiler
-echo "Building legate-profiler..."
 # shellcheck disable=SC2154
 GIT_COMMIT=$(git -C "${SRC_DIR}" rev-parse HEAD)
 echo "Legion checked-out with commit: ${GIT_COMMIT}"
 
-# Navigate to the legate-profiler directory and build it
+BUILD_DIR="${SRC_DIR}/build"
+TMP_INSTALL="${SRC_DIR}/tmp_install"
+mkdir -p "${BUILD_DIR}" "${TMP_INSTALL}"
+
+if [[ "${BUILD_WITH_CUDA:-0}" == '1' ]]; then
+    CUDA_ARGS="-DLegion_USE_CUDA=ON -DCMAKE_CUDA_COMPILER=${CUDACXX}"
+else
+    CUDA_ARGS="-DLegion_USE_CUDA=OFF"
+fi
+
+cd "${BUILD_DIR}"
+cmake "${CUDA_ARGS}" -DCMAKE_INSTALL_PREFIX="${TMP_INSTALL}" -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DLegion_BUILD_RUST_PROFILER=ON -DLegion_BUILD_EXAMPLES=ON -DLegion_USE_NVTX=ON ..
+make -j 16
+make install
+# 'PREFIX' is set by conda-build at runtime
 # shellcheck disable=SC2154
-LIBCLANG_PATH=${BUILD_PREFIX}/lib cargo install --path "${SRC_DIR}"/tools/legion_prof_rs --all-features --root "${PREFIX}"
+cp "${TMP_INSTALL}/bin/legion_prof" "${PREFIX}/bin/"
+
+rm -rf "${TMP_INSTALL}"
+
 echo "Done"
