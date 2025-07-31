8e73570a96ffecdae160267ff333900d468efcbc
Jacob Faibussowitsch <jacob.fai@gmail.com>
2025-07-31 15:52:46 -0400

    Bump aedifix version (#2588)

--- config/ensure_aedifix.py
+++ config/ensure_aedifix.py
@@ -18,7 +18,7 @@ except (ModuleNotFoundError, ImportError):
 
 def ensure_aedifix() -> None:
     # update pre-commit-config.yml as well in case this is changed
-    VERSION = Version("1.9.0")
+    VERSION = Version("1.10.0")
 
     try:
         mod_version = Version(version("aedifix"))
