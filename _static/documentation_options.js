2aa7926ecf279cdaa05961738fd68d627286185e
marcinz <marcinz@users.noreply.github.com>
2025-08-02 05:48:39 +0000

    Deploying to gh-pages from @ nv-legate/legate.internal@01382e717da803ba2b022a05cb7e43a95e3db04d 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '25.08.00.dev45+g78ac7918',
+    VERSION: '25.08.00.dev46+g01382e71',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
