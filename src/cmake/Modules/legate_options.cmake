c04570f30d407207c5f2e870240273f2cf3915bf
Marcin Zalewski <marcin.zalewski@gmail.com>
2025-09-10 08:15:26 -0700

    CMake: use mixed-case Legion_USE_GASNet for GEX (#2814) (#2818)

--- src/cmake/Modules/legate_options.cmake
+++ src/cmake/Modules/legate_options.cmake
@@ -95,7 +95,7 @@ legate_option(legate_USE_CPROFILE LEGATE_USE_CPROFILE "Enable Cprofile in Legate
 legate_option(legate_USE_NCCL LEGATE_USE_NCCL "Enable NCCL support" OFF)
 legate_option(legate_USE_UCX LEGATE_USE_UCX "Enable UCX support" OFF)
 legate_option(legate_USE_MPI LEGATE_USE_MPI "Enable MPI support" ${legate_USE_UCX})
-legate_option(Legion_USE_GASNET LEGION_USE_GASNET "Enable GASNet support in Legion" OFF)
+legate_option(Legion_USE_GASNet LEGION_USE_GASNET "Enable GASNet support in Legion" OFF)
 
 set(legion_networks)
 
@@ -105,7 +105,7 @@ endif()
 if(legate_USE_MPI)
   list(APPEND legion_networks "mpi")
 endif()
-if(Legion_USE_GASNET)
+if(Legion_USE_GASNet)
   list(APPEND legion_networks "gasnetex")
 endif()
 
