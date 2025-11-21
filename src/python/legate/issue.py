1632a278b1c57ff88966e81f773303d20d274f3d
tisaac <tisaac@nvidia.com>
2025-11-21 10:15:07 -0500

    Feature benchmark logs (#3119)
    * Make interface dictionary types for info
    
    * Add legate.util.benchmark
    
    * Add tests of benchmark_log()
    
    * Refactor BenchmarkLog to use impl type
    
    * Add test of empty fields in benchmark_log().log()
    
    * Fix missing file closure from refactor
    
    * Avoid starting the runtim in util.info
    
    * Remove unneeded members from refactor
    
    * Fix spaces in test_benchmark_metadata
    
    * Use f-string for filename construction in benchmark.py
    
    * Simplify output specification in benchmark_log(): just accept a TextIO
    
    * Refactor benchmark.py into multiple files
    
    * Add Docstrings for benchmarking methods
    
    * Make a TypedDict for build_info()
    
    * Update LEGATE_BENCMARCK_OUT documentation
    
    * Fix types and formatting in benchmark_info()
    
    * use ExitStack.pop_all() to safely set up the exit stack

--- src/python/legate/issue.py
+++ src/python/legate/issue.py
@@ -5,113 +5,13 @@ from __future__ import annotations
 
 # ruff: noqa: D103, T201
 import os
-import re
-import sys
-import json
-import platform
-from importlib import import_module
-from subprocess import CalledProcessError, check_output
-from textwrap import indent
 
-from .util.info import print_build_info
-
-FAILED_TO_DETECT = "(failed to detect)"
-
-
-def try_version(module_name: str, attr: str) -> str:
-    try:
-        module = import_module(module_name)
-        if not module:
-            return FAILED_TO_DETECT
-        return getattr(module, attr)
-    except ModuleNotFoundError:
-        return FAILED_TO_DETECT
-    except ImportError as e:
-        err = re.sub(r" \(.*\)", "", str(e))  # remove any local path
-        return f"(ImportError: {err})"
-    except Exception as e:
-        return f"(Exception on import: {e})"
-
-
-def legion_version() -> str:
-    import legate.install_info as info  # noqa: PLC0415
-
-    result = info.legion_version
-    if result == "":
-        return FAILED_TO_DETECT
-
-    if info.legion_git_branch:
-        result += f" (commit: {info.legion_git_branch})"
-
-    return result
-
-
-def try_conda(package: str) -> str:
-    try:
-        if out := check_output(["conda", "list", package, "--json"]):
-            info = json.loads(out.decode("utf-8"))[0]
-            return f"{info['dist_name']} ({info['channel']})"
-
-    except (CalledProcessError, IndexError, KeyError):
-        return FAILED_TO_DETECT
-    except FileNotFoundError:
-        return "(conda missing)"
-    else:
-        return FAILED_TO_DETECT
-
-
-def print_system_info() -> None:
-    print("System info:")
-    print(f"  Python      :  {sys.version.splitlines()[0]}")
-    print(f"  Platform    :  {platform.platform()}")
-    print(f"  GPU driver  :  {driver_version()}")
-    print(f"  GPU devices :  {devices()}")
-
-
-def print_package_versions() -> None:
-    print("Package versions:")
-    print(f"  legion      :  {legion_version()}")
-    print(f"  legate      :  {try_version('legate', '__version__')}")
-    print(f"  cupynumeric :  {try_version('cupynumeric', '__version__')}")
-    print(f"  numpy       :  {try_version('numpy', '__version__')}")
-    print(f"  scipy       :  {try_version('scipy', '__version__')}")
-    print(f"  numba       :  {try_version('numba', '__version__')}")
-
-
-def print_pacakge_details() -> None:
-    print("Package details:")
-    packages = ("cuda-version", "legate", "cupynumeric")
-    N = max(len(pkg) for pkg in packages)
-    for pkg in packages:
-        print(f"  {pkg:<{N + 1}}: {try_conda(pkg)}")
-
-
-def driver_version() -> str:
-    cmd = (
-        "nvidia-smi",
-        "--query-gpu=driver_version",
-        "--format=csv,noheader",
-        "--id=0",
-    )
-    try:
-        out = check_output(cmd)
-        return out.decode("utf-8").strip()
-    except (CalledProcessError, IndexError, KeyError):
-        return FAILED_TO_DETECT
-    except FileNotFoundError:
-        return "(nvidia-smi missing)"
-
-
-def devices() -> str:
-    cmd = ["nvidia-smi", "-L"]
-    try:
-        out = check_output(cmd)
-        gpus = re.sub(r" \(UUID: .*\)", "", out.decode("utf-8").strip())
-        return f"\n{indent(gpus, '    ')}"
-    except (CalledProcessError, IndexError, KeyError):
-        return FAILED_TO_DETECT
-    except FileNotFoundError:
-        return "(nvidia-smi missing)"
+from .util.info import (
+    print_build_info,
+    print_package_details,
+    print_package_versions,
+    print_system_info,
+)
 
 
 def main() -> None:
@@ -119,10 +19,14 @@ def main() -> None:
     # too aggressive and will cause legate-issue itself to crash
     os.environ["LEGATE_AUTO_CONFIG"] = "0"
 
+    # we could just call _nested_pretty_print(info()), but
+    # there is a noticeable delay in generating the package details
+    # from calling conda, and we want to start printing information
+    # as quickly as possible
     print_system_info()
     print()
     print_package_versions()
     print()
-    print_pacakge_details()
-    print()
     print_build_info()
+    print()
+    print_package_details()
---
+++ tests/python/integration/test_benchmark.py
@@ -0,0 +1,137 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025-2025 NVIDIA CORPORATION & AFFILIATES.
+#                         All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+from __future__ import annotations
+
+import io
+import re
+import csv
+import sys
+from typing import Any, TextIO
+
+from legate.util.benchmark import benchmark_log
+
+
+def read_commented_csv(f: TextIO) -> dict[str, list[str]]:
+    d: dict[str, list[str]] = {}
+    reader = csv.DictReader(filter(lambda row: row[0] != "#", f))
+    for row in reader:
+        for k, v in row.items():
+            if k in d:
+                d[k].append(v)
+            else:
+                d[k] = [v]
+    return d
+
+
+SIZES = [1, 10, 100, 1000]
+TIMES = [0.5, 1.0, 2.0, 4.0]
+
+
+def write_string_dict(**kwargs: list[Any]) -> dict[str, list[str]]:
+    d: dict[str, list[str]] = {}
+    for k, v in kwargs.items():
+        d[k] = [str(i) for i in v]
+    return d
+
+
+def test_benchmark_basic() -> None:
+    outfile = io.StringIO()
+    d_direct = write_string_dict(size=SIZES, time=TIMES)
+    with benchmark_log("basic", ["size", "time"], out=outfile) as b:
+        for s, t in zip(SIZES, TIMES, strict=True):
+            b.log(time=t, size=s)
+    string = outfile.getvalue()
+    infile = io.StringIO(string)
+    d_from_file = read_commented_csv(infile)
+    assert d_direct == d_from_file
+
+
+def test_benchmark_rich() -> None:
+    # this test only fails if rich emits an error, and you can
+    # observe rich output with pytest options `-s -k test_benchmark_rich`,
+    # but I'm not sure how to write a test to verify rich output
+    with benchmark_log("basic", ["size", "time"], out=sys.stdout) as b:
+        for s, t in zip(SIZES, TIMES, strict=True):
+            b.log(time=t, size=s)
+
+
+def test_benchmark_fields_with_spaces() -> None:
+    outfile = io.StringIO()
+    size = "size (florps)"
+    time = "time (seconds)"
+    d_direct = write_string_dict(**{size: SIZES, time: TIMES})  # type: ignore[arg-type]
+    with benchmark_log("basic (with spaces)", [size, time], out=outfile) as b:
+        for s, t in zip(SIZES, TIMES, strict=True):
+            b.log(**{time: t, size: s})
+    string = outfile.getvalue()
+    infile = io.StringIO(string)
+    d_from_file = read_commented_csv(infile)
+    assert d_direct == d_from_file
+
+
+def test_benchmark_empty() -> None:
+    sizes_with_empty = ["1", "10", "(missing)", "(missing)"]
+    times_with_empty = ["0.5", "(missing)", "2.0", "(missing)"]
+    outfile = io.StringIO()
+    d_direct = write_string_dict(size=sizes_with_empty, time=times_with_empty)
+    with benchmark_log("basic", ["size", "time"], out=outfile) as b:
+        b.log(size=1, time=0.5)
+        b.log(size=10)
+        b.log(time=2.0)
+        b.log()
+    string = outfile.getvalue()
+    infile = io.StringIO(string)
+    d_from_file = read_commented_csv(infile)
+    assert d_direct == d_from_file
+
+
+def test_metadata() -> None:
+    outfile = io.StringIO()
+    with benchmark_log("basic", ["size", "time"], out=outfile):
+        pass
+    string = outfile.getvalue()
+    infile = io.StringIO(string)
+    header = filter(lambda r: r[0] == "#", infile)
+    metadata = "".join(line.strip("#") for line in header)
+    expected = [
+        "Benchmark *:",
+        "  Name *:",
+        "  ID *:",
+        "  Time *:",
+        "  Global rank *:",
+        "  Local rank *:",
+        "  Node *:",
+        "Program *:",
+        "Legate runtime configuration *:",
+        "Machine *:",
+        "  Preferred target *:",
+        "  GPU *:",
+        "  OMP *:",
+        "  CPU *:",
+        "System info *:",
+        "  Python *:",
+        "  Platform *:",
+        "  GPU driver *:",
+        "  GPU devices *:",
+        "Package versions *:",
+        "  legion *:",
+        "  legate *:",
+        "  cupynumeric *:",
+        "  numpy *:",
+        "  scipy *:",
+        "  numba *:",
+        "Package details *:",
+        "  cuda-version *:",
+        "  legate *:",
+        "  cupynumeric *:",
+        "Legate build configuration *:",
+        "  build_type *:",
+        "  use_openmp *:",
+        "  use_cuda *:",
+        "  networks *:",
+        "  conduit *:",
+        "  configure_options *:",
+    ]
+    for entry in expected:
+        assert re.search(entry, metadata) is not None
