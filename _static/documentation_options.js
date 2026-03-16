b9ff5b84a96701b3b90b03bde68db0fd4b70abee
XiaLuNV <XiaLuNV@users.noreply.github.com>
2026-03-16 09:50:14 +0000

    Deploying to gh-pages from @ nv-legate/legate.internal@c9c286df2082b61055312b613bafa8db3f702491 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '26.05.00.dev17+gc9c286df2',
+    VERSION: '26.01.00.dev107+g4784e8658',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
