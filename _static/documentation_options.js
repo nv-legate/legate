a1828be0ec96d6aea77f6c693a0dca4db933582c
supunkamburugamuve <supunkamburugamuve@users.noreply.github.com>
2026-03-18 18:37:35 +0000

    Deploying to gh-pages from @ nv-legate/legate.internal@e47c55b38839ba23717c57dea44314ca9c266dbc 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '26.05.00.dev17+gc9c286df2',
+    VERSION: '26.05.00.dev19+ge47c55b38',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
