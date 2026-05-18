nv-legate/legate

commit 8c28e4543c3e00f08e18816e55195896cb1ea00a

NCCL: Cleanup get_nccl.cmake to work with NCCL CMake builds (#3757)

src/cmake/thirdparty/get_nccl.cmake
@@ -5,134 +5,83 @@
 
 include_guard(GLOBAL)
 
-function(validate_nccl_version)
-  if(NOT legate_nccl_include_dir)
-    get_target_property(nccl_include_dirs NCCL::NCCL INTERFACE_INCLUDE_DIRECTORIES)
-
-    set(candidate_incdirs)
-    foreach(potential_dir IN LISTS nccl_include_dirs)
-      string(GENEX_STRIP "${potential_dir}" stripped_dir)
-      list(APPEND candidate_incdirs "${stripped_dir}")
-    endforeach()
-
-    find_path(
-      legate_nccl_include_dir
-      NAMES nccl.h
-      PATH_SUFFIXES nccl
-      HINTS ${candidate_incdirs}
-      DOC "Path containing nccl.h"
-    )
+function(get_nccl_version ver_var)
+  find_file(
+    NCCL_INCLUDE_PATH
+    NAMES nccl.h
+    PATHS ${NCCL_INCLUDE_DIRS}
+    REQUIRED
+    NO_DEFAULT_PATH
+  )
+  file(
+    STRINGS "${NCCL_INCLUDE_PATH}"
+    file_ver
+    LIMIT_COUNT 1
+    REGEX [=[^#define[ \t]+NCCL_VERSION_CODE[ \t]+[0-9]+]=]
+  )
+  if(NOT file_ver)
+    message(FATAL_ERROR "Could not read NCCL version from ${NCCL_INCLUDE_PATH}")
   endif()
+  string(REGEX MATCH [=[[0-9]+]=] file_ver "${file_ver}")
 
-  if(NOT legate_nccl_include_dir)
+  string(LENGTH "${file_ver}" ver_len)
+  if(ver_len LESS 4 OR ver_len GREATER 5)
     message(
       FATAL_ERROR
-      "Could not find nccl.h in any include directory for "
-      "NCCL version validation. Searched: ${candidate_incdirs}"
+      "Could not parse NCCL version ${file_ver} from ${NCCL_INCLUDE_PATH}"
     )
   endif()
-
-  set(nccl_header_path "${legate_nccl_include_dir}/nccl.h")
-
-  file(
-    STRINGS "${nccl_header_path}"
-    maj_line
-    LIMIT_COUNT 1
-    REGEX [=[^#define[ \t]+NCCL_MAJOR[ \t]+[0-9]+]=]
-  )
-  file(
-    STRINGS "${nccl_header_path}"
-    min_line
-    LIMIT_COUNT 1
-    REGEX [=[^#define[ \t]+NCCL_MINOR[ \t]+[0-9]+]=]
-  )
-
-  string(REGEX MATCH [=[[0-9]+]=] nccl_major "${maj_line}")
-  string(REGEX MATCH [=[[0-9]+]=] nccl_minor "${min_line}")
-
-  if(nccl_major STREQUAL "" OR nccl_minor STREQUAL "")
-    message(FATAL_ERROR "Could not read NCCL version from ${nccl_header_path}")
+  if(ver_len EQUAL 4)
+    math(EXPR ver_major "${file_ver}/1000")
+    math(EXPR ver_minor "(${file_ver}%1000)/100")
+  elseif(ver_len EQUAL 5)
+    math(EXPR ver_major "${file_ver}/10000")
+    math(EXPR ver_minor "(${file_ver}%10000)/100")
   endif()
+  math(EXPR ver_patch "${file_ver}%100")
+  set(${ver_var} "${ver_major}.${ver_minor}.${ver_patch}" PARENT_SCOPE)
+endfunction()
 
-  set(nccl_version "${nccl_major}.${nccl_minor}")
+function(validate_nccl_version ver)
   # NCCL >=2.28 is required for ncclAlltoAll. If you bump these, also bump
   # the dependency lists.
-  set(minimum_nccl_version 2.28)
-  set(maximum_nccl_version 2.29)
+  # ver_max is an *exclusive* upper bound
+  set(ver_min 2.28)
+  set(ver_max 2.30)
 
-  if("${nccl_version}" VERSION_LESS "${minimum_nccl_version}")
-    message(
-      FATAL_ERROR
-      "Detected NCCL version ${nccl_version}, but "
-      "version ${minimum_nccl_version} or newer is required."
-    )
+  if(ver VERSION_LESS ver_min)
+    message(FATAL_ERROR "Detected NCCL version ${ver}, but >= ${ver_min} is required")
   endif()
-
-  if("${maximum_nccl_version}" VERSION_LESS "${nccl_version}")
-    message(
-      FATAL_ERROR
-      "Detected NCCL version ${nccl_version}, but "
-      "version ${maximum_nccl_version} or lower is required."
-    )
+  if(ver VERSION_GREATER_EQUAL ver_max)
+    message(FATAL_ERROR "Detected NCCL version ${ver}, but < ${ver_max} is required")
   endif()
-
-  message(
-    STATUS
-    "NCCL version ${nccl_version} meets requirement "
-    ">= ${minimum_nccl_version}, <= ${maximum_nccl_version}"
-  )
+  message(STATUS "NCCL version ${ver} meets requirement >= ${ver_min}, < ${ver_max}")
 endfunction()
 
 function(find_or_configure_nccl)
   list(APPEND CMAKE_MESSAGE_CONTEXT "nccl")
 
-  if(TARGET NCCL::NCCL)
-    validate_nccl_version()
-    return()
-  endif()
-
-  rapids_find_generate_module(NCCL HEADER_NAMES nccl.h LIBRARY_NAMES nccl)
-  # Currently NCCL has no CMake build-system so we require it built and installed on the
-  # machine already
-  rapids_find_package(NCCL)
-  if(TARGET NCCL::NCCL)
-    validate_nccl_version()
-    return()
-  endif()
-
-  # If the user has installed NCCL to a system location, then CMake might not find their
-  # NCCL because it will be under /usr/lib/<whatever>. CMake will, however, search under
-  # there if CMAKE_LIBRARY_ARCHITECTURE is set, so we need to ensure that it is.
-  if(CMAKE_LIBRARY_ARCHITECTURE)
-    # In this case, the below find_package() will fail (we are doing the same thing as
-    # above without changing anything), so better to fail with a more useful error message
-    # instead.
-    message(VERBOSE "CMAKE_LIBRARY_ARCHITECTURE is set: ${CMAKE_LIBRARY_ARCHITECTURE}")
-    message(
-      FATAL_ERROR
-      "Could not find NCCL on your system. It's possible that you have it installed in a location that CMake does not know to search for automatically. Please report this case to Legate maintainers."
-    )
-  endif()
-
-  message(VERBOSE "CMAKE_LIBRARY_ARCHITECTURE is unset, attempting to deduce it")
-  if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
-    message(VERBOSE "linux system detected")
-    foreach(arch_dir IN ITEMS x86_64-linux-gnu aarch64-linux-gnu)
-      if(EXISTS "/usr/lib/${arch_dir}")
-        set(CMAKE_LIBRARY_ARCHITECTURE "${arch_dir}")
-        break()
+  if(NOT TARGET NCCL::NCCL)
+    # Workaround from #921 where find may fail when mixing conda and system deps
+    if(CMAKE_SYSTEM_NAME STREQUAL "Linux" AND NOT CMAKE_LIBRARY_ARCHITECTURE)
+      message(
+        VERBOSE
+        "linux system detected\n"
+        "CMAKE_LIBRARY_ARCHITECTURE is unset, attempting to deduce it"
+      )
+      if(EXISTS "/usr/lib/${CMAKE_SYSTEM_PROCESSOR}")
+        set(CMAKE_LIBRARY_ARCHITECTURE "${CMAKE_SYSTEM_PROCESSOR}")
       endif()
-    endforeach()
-  endif()
+    endif()
 
-  if(NOT CMAKE_LIBRARY_ARCHITECTURE)
-    message(
-      FATAL_ERROR
-      "Could not auto-deduce CMAKE_LIBRARY_ARCHITECTURE while trying to locate NCCL. It's possible that you have NCCL installed in a location that CMake does not know to search for automatically (which we tried to remedy by deducing CMAKE_LIBRARY_ARCHITECTURE). Please report this case to Legate maintainers."
-    )
+    rapids_find_generate_module(NCCL HEADER_NAMES nccl.h LIBRARY_NAMES nccl)
+    rapids_find_package(NCCL REQUIRED)
   endif()
-
-  message(VERBOSE "CMAKE_LIBRARY_ARCHITECTURE is set: ${CMAKE_LIBRARY_ARCHITECTURE}")
-  rapids_find_package(NCCL REQUIRED)
-  validate_nccl_version()
+  if(TARGET NCCL::nccl AND NOT TARGET NCCL::NCCL)
+    add_library(NCCL::NCCL ALIAS NCCL::nccl)
+  endif()
+  if(NOT NCCL_VERSION)
+    get_nccl_version(NCCL_VERSION)
+  endif()
+  validate_nccl_version(${NCCL_VERSION})
 endfunction()

commit 44048531bf89aa0af9ae86278a4682b027add3bb

NCCL: Fix various cmake workarounds that broke in #3757 (#3760)

src/cmake/thirdparty/get_nccl.cmake
@@ -6,6 +6,14 @@
 include_guard(GLOBAL)
 
 function(get_nccl_version ver_var)
+  if(NOT NCCL_INCLUDE_DIRS)
+    if(NCCL_INCLUDE_DIR)
+      set(NCCL_INCLUDE_DIRS ${NCCL_INCLUDE_DIR})
+    else()
+      get_target_property(NCCL_INCLUDE_DIRS NCCL::NCCL INTERFACE_INCLUDE_DIRECTORIES)
+    endif()
+  endif()
+
   find_file(
     NCCL_INCLUDE_PATH
     NAMES nccl.h
@@ -24,19 +32,19 @@ function(get_nccl_version ver_var)
   endif()
   string(REGEX MATCH [=[[0-9]+]=] file_ver "${file_ver}")
 
+  # The following version number parsing is taken directly from nccl.h
   string(LENGTH "${file_ver}" ver_len)
-  if(ver_len LESS 4 OR ver_len GREATER 5)
-    message(
-      FATAL_ERROR
-      "Could not parse NCCL version ${file_ver} from ${NCCL_INCLUDE_PATH}"
-    )
-  endif()
   if(ver_len EQUAL 4)
     math(EXPR ver_major "${file_ver}/1000")
     math(EXPR ver_minor "(${file_ver}%1000)/100")
   elseif(ver_len EQUAL 5)
     math(EXPR ver_major "${file_ver}/10000")
     math(EXPR ver_minor "(${file_ver}%10000)/100")
+  else()
+    message(
+      FATAL_ERROR
+      "Could not parse NCCL version ${file_ver} from ${NCCL_INCLUDE_PATH}"
+    )
   endif()
   math(EXPR ver_patch "${file_ver}%100")
   set(${ver_var} "${ver_major}.${ver_minor}.${ver_patch}" PARENT_SCOPE)
@@ -63,15 +71,17 @@ function(find_or_configure_nccl)
 
   if(NOT TARGET NCCL::NCCL)
     # Workaround from #921 where find may fail when mixing conda and system deps
-    if(CMAKE_SYSTEM_NAME STREQUAL "Linux" AND NOT CMAKE_LIBRARY_ARCHITECTURE)
+    if(
+      CMAKE_SYSTEM_NAME STREQUAL "Linux"
+      AND NOT CMAKE_LIBRARY_ARCHITECTURE
+      AND EXISTS "/usr/lib/${CMAKE_SYSTEM_PROCESSOR}-linux-gnu"
+    )
       message(
         VERBOSE
         "linux system detected\n"
         "CMAKE_LIBRARY_ARCHITECTURE is unset, attempting to deduce it"
       )
-      if(EXISTS "/usr/lib/${CMAKE_SYSTEM_PROCESSOR}")
-        set(CMAKE_LIBRARY_ARCHITECTURE "${CMAKE_SYSTEM_PROCESSOR}")
-      endif()
+      set(CMAKE_LIBRARY_ARCHITECTURE "${CMAKE_SYSTEM_PROCESSOR}-linux-gnu")
     endif()
 
     rapids_find_generate_module(NCCL HEADER_NAMES nccl.h LIBRARY_NAMES nccl)